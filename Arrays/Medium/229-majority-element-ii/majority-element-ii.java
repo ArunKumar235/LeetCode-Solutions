2ef04e4016590f53d6de5d8219781825831bf88e
Arun Kumar M <marunkumar0203@gmail.com>
2024-07-22 21:44:18 +0530

    Time: 2 ms (99.20%) | Memory: 47.9 MB (53.77%) - LeetSync

--- Arrays/Medium/229-majority-element-ii/majority-element-ii.java
+++ Arrays/Medium/229-majority-element-ii/majority-element-ii.java
@@ -1,27 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // Create a frequency map to store the count of each element
-        Map<Integer, Integer> elementCountMap = new HashMap<>();
+        int n = nums.length;
         
-        // Iterate through the input array to count element occurrences
-        for (int i = 0; i < nums.length; i++) {
-            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        List<Integer> majorityElements = new ArrayList<>();
-        int threshold = nums.length / 3;
-        
-        // Iterate through the frequency map to identify majority elements
-        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
-            int element = entry.getKey();
-            int count = entry.getValue();
-            
-            // Check if the element count is greater than the threshold
-            if (count > threshold) {
-                majorityElements.add(element);
+        //Initialize the count
+        int cnt1 = 0;
+        int cnt2 = 0;
+        //Initialize the elements to track
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+
+        //Applying the extended version of the algorithm of >n/2 question
+        //in case having trouble visit that once to understand it properly
+        for(int i=0; i<n; i++){
+            if(cnt1==0 && ele2 != nums[i]){
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2==0 && ele1 != nums[i]){
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
             }
         }
-        
-        return majorityElements;
+        List<Integer> list = new ArrayList<>(); //List of Answers
+
+        //Manually check if the storedelements in ele1 and ele2 
+        //are the majority element:
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
+        }
+
+        int min = (int)(n/3) + 1; //min majority element nikalne ke liye
+        if(cnt1>=min) list.add(ele1);
+        if(cnt2>=min) list.add(ele2);
+
+        return list;
     }
 }
