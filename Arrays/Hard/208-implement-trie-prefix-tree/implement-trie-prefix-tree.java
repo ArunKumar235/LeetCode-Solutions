426ba8244ef286f837453b93282853abe17bb73b
Arun Kumar M <marunkumar0203@gmail.com>
2024-07-26 00:30:06 +0530

    Time: 34 ms (77.36%) | Memory: 55.8 MB (10.35%) - LeetSync

--- Arrays/Hard/208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ Arrays/Hard/208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,66 +1,62 @@
 class Trie {
-
-    static Node root;
-
+    Node root;
 
     public Trie() {
         root = new Node();
     }
 
-
-    class Node{
-        Node[] children;
-        boolean eow;
-        Node(){
-            children = new Node[26];
-            for(int i =0; i<26; i++){
-                children[i] = null;
-            }
-            eow = false;
-        }
-    }   
-    
-
     public void insert(String word) {
-        Node curr = root;
-        for(int i =0; i<word.length(); i++){
-            int index = word.charAt(i) - 'a';
-            if(curr.children[index] == null){
-                curr.children[index] = new Node();
-            }
-            if(i==word.length()-1){
-                curr.children[index].eow = true;
-            }
-            curr = curr.children[index];
-        }
-        
+        root.insert(word, 0);
     }
-    
+
     public boolean search(String word) {
-        Node curr = root;
-        for(int i = 0; i<word.length(); i++){
-            int index = word.charAt(i) - 'a';
-            if(curr.children[index] == null){
-                return false;
-            }
-            if(i==word.length()-1 && curr.children[index].eow == false){
-                return false;
-            }
-            curr = curr.children[index];
-        }
-        return true;
+        return root.search(word, 0);
     }
-    
+
     public boolean startsWith(String prefix) {
-        Node curr = root;
-        for(int i = 0; i<prefix.length(); i++){
-            int index = prefix.charAt(i) - 'a';
-            if(curr.children[index] == null){
-                return false;
-            }
-            curr = curr.children[index];
+        return root.startsWith(prefix, 0);
+    }
+
+    class Node {
+        Node[] nodes;
+        boolean isLast;
+
+        public Node() {
+            nodes = new Node[26];
         }
-        return true;
+
+        public void insert(String word, int idx) {
+            if (idx >= word.length())
+                return;
+            int i = word.charAt(idx) - 'a';
+            if (nodes[i] == null)
+                nodes[i] = new Node();
+            if (idx == word.length() - 1)
+                nodes[i].isLast = true;
+            nodes[i].insert(word, idx + 1);
+        }
+
+        public boolean search(String word, int idx) {
+            if (idx >= word.length())
+                return false;
+            Node node = nodes[word.charAt(idx) - 'a'];
+            if (node == null)
+                return false;
+            if (idx == word.length() - 1 && node.isLast)
+                return true;
+            return node.search(word, idx + 1);
+        }
+
+        public boolean startsWith(String prefix, int idx) {
+            if (idx >= prefix.length())
+                return false;
+            Node node = nodes[prefix.charAt(idx) - 'a'];
+            if (node == null)
+                return false;
+            if (prefix.length() - 1 == idx)
+                return true;
+            return node.startsWith(prefix, idx + 1);
+        }
     }
 }
 
