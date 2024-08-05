348612f9910a2d9652b4155e90ca906ad9b38570
Arun Kumar M <marunkumar0203@gmail.com>
2024-08-05 12:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (10.38%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,36 +1,20 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) {
-            return head;
-        }
-
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(0); // created dummy node
         dummy.next = head;
-        ListNode prev = dummy;
-
-        for (int i = 0; i < left - 1; i++) {
-            prev = prev.next;
+        ListNode prev = dummy; // intialising prev pointer on dummy node
+        
+        for(int i = 0; i < left - 1; i++)
+            prev = prev.next; // adjusting the prev pointer on it's actual index
+        
+        ListNode curr = prev.next; // curr pointer will be just after prev
+        // reversing
+        for(int i = 0; i < right - left; i++){
+            ListNode forw = curr.next; // forw pointer will be after curr
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
         }
-
-        ListNode cur = prev.next;
-
-        for (int i = 0; i < right - left; i++) {
-            ListNode temp = cur.next;
-            cur.next = temp.next;
-            temp.next = prev.next;
-            prev.next = temp;
-        }
-
-        return dummy.next;        
+        return dummy.next;
     }
 }
