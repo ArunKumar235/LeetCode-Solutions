24026fc55db1ff6ec53cf1f1f0cd8b8d6fa076e6
Arun Kumar M <marunkumar0203@gmail.com>
2024-08-11 16:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (33.18%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,53 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int len = mountainArr.length() - 1;
+        int peak = findPeak(0, len, mountainArr);
+        int ans = binarySearch(0,peak,mountainArr, target);
+        if (ans != -1){
+            return ans;
+        }
+        return agonisticBinarySearch(peak,len,mountainArr, target);
+    }
+
+    public int findPeak(int start, int end, MountainArray mountainArr){
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return end;
+    }
+    
+    public int binarySearch(int start, int end, MountainArray mountainArr, int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mountainArr.get(mid) == target) return mid;
+            if(mountainArr.get(mid)<target) start = mid+1;
+            else end = mid-1;
+        }
+        return -1;
+    }
+
+    public int agonisticBinarySearch(int start, int end, MountainArray mountainArr, int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(mountainArr.get(mid)==target) return mid;
+            if(mountainArr.get(mid)<target) end = mid-1;
+            else start = mid + 1;
+        }
+        return -1;
+    }
+}
