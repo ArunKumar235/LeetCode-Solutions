23d4a6389e775ed6f75e3463e5a08c28df01b4f1
Arun Kumar M <marunkumar0203@gmail.com>
2024-08-31 10:31:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (9.54%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,15 @@
 class Solution {
-    HashMap<Integer, Integer> memo = new HashMap<>();
     public int rob(int[] nums) {
-        return helper(nums, 0);
-    }
-
-    public int helper(int[] nums, int index){
-        if(index>=nums.length) return 0;
-        if(memo.containsKey(index)) return memo.get(index);
-        int result = Math.max(nums[index]+helper(nums, index+2), helper(nums, index+1));
-        memo.put(index, result);
-        return result;
+        if(nums.length==1){
+            return nums[0];
+        }
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        ans[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i<nums.length; i++){
+            ans[i] = Math.max(ans[i-1], nums[i] + ans[i-2]);
+        }
+        // System.out.println(Arrays.toString(ans));
+        return ans[nums.length-1];
     }
 }
