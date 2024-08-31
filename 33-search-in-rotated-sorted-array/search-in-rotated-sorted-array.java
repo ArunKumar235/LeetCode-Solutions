cbd4f8829ae856383cd260b66aca8d93eaff338f
Arun Kumar M <marunkumar0203@gmail.com>
2024-08-31 13:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (7.42%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,49 +1,40 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[start]<=nums[mid]){
-                if(target >= nums[start] && target<=nums[mid]){
-                    end = mid-1;
-                }else{
-                    start = mid+1;
-                }
-            }else if(target >= nums[mid] && target<=nums[end]){
-                start = mid+1;
-            }else{
-                end = mid-1;
+        // int start = 0;
+        // int end = nums.length-1;
+        // while(start<=end){
+        //     int mid = start + (end-start)/2;
+        //     if(nums[mid]==target) return mid;
+        //     if(nums[start]<=nums[mid]){
+        //         if(target >= nums[start] && target<=nums[mid]){
+        //             end = mid-1;
+        //         }else{
+        //             start = mid+1;
+        //         }
+        //     }else if(target >= nums[mid] && target<=nums[end]){
+        //         start = mid+1;
+        //     }else{
+        //         end = mid-1;
+        //     }
+        // }
+        // return -1;
+
+        return search(nums, 0, nums.length-1, target);
+        
+    }
+
+    public int search(int[] arr, int start, int end, int target){
+        if(start>end) return -1;
+        int mid = start + (end-start)/2;
+        if(arr[mid]==target) return mid;
+        if(arr[start]<=arr[mid]){
+            if((target >= arr[start] && target <= arr[mid])){
+                return search(arr, start, mid-1, target);
             }
+            return search(arr, mid+1, end, target);
         }
-        return -1;
-
-
-        // int len = nums.length-1;
-        // int pivot = findPivot(0, len, nums);
-        // int ans = binarySearch(0, pivot, nums, target);
-        // return (ans!=-1) ? ans : binarySearch(pivot+1, len, nums, target);
-    }
-
-    public int findPivot( int start, int end, int[] nums){
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]>nums[mid+1]) return mid;
-            if(nums[mid]>=nums[start]) start = mid+1;
-            else end = mid;
-        }
-        return start;
-    }
-
-    public int binarySearch(int start, int end, int[] nums, int target){
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target) start = mid+1;
-            else end = mid-1;
-        }
-        return -1;
+        if(target>=arr[mid] && target<=arr[end]) return search(arr, mid+1, end, target);
+        return search(arr, start, mid-1, target);
     }
 }
