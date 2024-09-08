5ce59eed352126b74f4472e31ed88a7f68dd1e1b
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-08 23:50:27 +0530

    Time: 49 ms (100.00%) | Memory: 63.3 MB (100.00%) - LeetSync

---
+++ 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxPossibleScore(int[] start, int d) {
+        Arrays.sort(start);
+        int n = start.length;
+
+        long left = 0, right = 2_000_000_000L;
+        while (left < right) {
+            long mid = (right + left + 1) / 2;
+            if (canChoose(start, d, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int) left;
+    }
+
+    private static boolean canChoose(int[] start, int d, long minDiff) {
+        long chosen = start[0];
+        for (int i = 1; i < start.length; i++) {
+            if (chosen + minDiff > start[i] + d) {
+                return false;
+            }
+            chosen = Math.max(chosen + minDiff, start[i]);
+        }
+        return true;
+    }
+}
