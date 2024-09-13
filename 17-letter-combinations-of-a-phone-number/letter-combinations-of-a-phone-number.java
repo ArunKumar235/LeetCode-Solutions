1952f232d9b8203ca320e97ec8174cebea02725b
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-13 10:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (99.87%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,34 +1,36 @@
 class Solution {
-    
-    public Map<Character, String> letterMap = Map.of(
-            '2', "abc",
-            '3', "def",
-            '4', "ghi",
-            '5', "jkl",
-            '6', "mno",
-            '7', "pqrs",
-            '8', "tuv",
-            '9', "wxyz"
-    );
-    
     public List<String> letterCombinations(String digits) {
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
         List<String> result = new ArrayList<>();
-        if(digits.isEmpty()) return result;
+        if(digits.isEmpty()){
+            return result;
+        }
         StringBuilder sb = new StringBuilder();
-        solve(digits, 0, sb, result);
+        solve(digits, 0, sb, result, map);
         return result;
     }
 
-    public void solve(String digits, int index, StringBuilder sb, List<String> result){
-        if(digits.length()==index){
+    public void solve(String digits, int index, StringBuilder sb, List<String> result, HashMap<Character, String> map){
+        if(index==digits.length()){
             result.add(sb.toString());
             return;
         }
-        String str = letterMap.get(digits.charAt(index));
-        for(int i =0 ;i<str.length(); i++){
-            sb.append(str.charAt(i));
-            solve(digits, index+1, sb, result);
+        char ch = digits.charAt(index);
+        String rep = map.get(ch);
+        for(int i = 0; i<rep.length(); i++){
+            sb.append(rep.charAt(i));
+            solve(digits, index+1, sb, result, map);
             sb.setLength(sb.length()-1);
         }
+
+
     }
 }
