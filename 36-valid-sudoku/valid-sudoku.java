50de17b3eade16760e014d4d27b10d154cbca3d9
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-14 13:31:10 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (71.08%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                char number = board[i][j];
-                if (number != '.')
-                    if (!seen.add(number + " in row " + i) ||
-                        !seen.add(number + " in column " + j) ||
-                        !seen.add(number + " in block " + i/3 + "-" + j/3))
-                        return false;
+        int[] rFlag = new int[9];
+        int[] cFlag = new int[9];
+        int[] smFlag = new int[9];
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j]=='.') continue;
+                int val = board[i][j]-'1';
+                int smidx = (i/3)*3 + (j/3);
+                if((rFlag[i]&(1<<val))!=0) return false;
+                if((cFlag[j]&(1<<val))!=0) return false;
+                if((smFlag[smidx]&(1<<val))!=0) return false;
+                rFlag[i] |= 1<<val;
+                cFlag[j] |= 1<<val;
+                smFlag[smidx] |= 1<<val;
             }
         }
         return true;
