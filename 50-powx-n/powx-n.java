07dcd873704f82cdfb984e8237a0619f6e99f384
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-25 18:49:01 +0530

    Time: 1 ms (6.67%) | Memory: 41.6 MB (89.24%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,23 +1,20 @@
 class Solution {
-     public double myPow(double x, int n) {
-        if (x == 1) return 1;
-        long longN = n;
-        double ans = solve(x, Math.abs(longN));
-        if (longN < 0)
-            return 1 / ans;
-        return ans;
-    }
-
-    public double solve(double x, long n) {
-        double ans = 1;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                ans *= x;
+    public double myPow(double x, int N) {
+        if(x==1) return 1;
+        long n = N;
+        double res = 1;
+        boolean isneg = n<0;
+        n = Math.abs(n);
+        while(n>0){
+            if((n&1)==1){
+                res*=x;
             }
-            x *= x;
-            n >>= 1;
+            x*=x;
+            n = n>>1;
         }
-        return ans;
+        if(isneg){
+            return 1.0/res;
+        }
+        return res;
     }
-    
 }
