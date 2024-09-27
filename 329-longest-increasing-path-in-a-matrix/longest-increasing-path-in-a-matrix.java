d52d1882ff61335bb3b7808a46cf0839f61eebcf
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-27 13:51:52 +0530

    Time: 8 ms (83.46%) | Memory: 44.8 MB (68.54%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,35 +1,33 @@
 class Solution {
-    int[] rowDiff = {-1, 0, 1,  0};
-    int[] colDiff = { 0, 1, 0, -1};
-
     public int longestIncreasingPath(int[][] mat) {
         int R = mat.length;
         int C = mat[0].length;
         int[][] dp = new int[R][C];
-        int max = 1;
-        for(int i = 0; i< R; i++){
-            for(int j = 0; j< C; j++){
-                dp[i][j] = dfs(mat, dp, i, j, R, C);
+        int max = -1;
+        for(int i = 0;i <R; i++){
+            for(int j = 0; j<C; j++){
+                dp[i][j] = dfs(mat,dp,i,j,R,C,-1);
                 max = Math.max(max, dp[i][j]);
             }
         }
         return max;
     }
-    public int dfs(int[][] mat, int[][]dp, int row, int col, int R, int C){
+    public int dfs(int[][] mat, int[][] dp, int row, int col, int R, int C, int prev){
+        if(row>=R || row<0 || col>=C || col<0){
+            return 0;
+        }
+        if(mat[row][col]<=prev){
+            return 0;
+        }
         if(dp[row][col]!=0){
             return dp[row][col];
         }
-        int max = 1;
-        for(int i = 0; i<4; i++){
-            int cRow = row+rowDiff[i];
-            int cCol = col+colDiff[i];
-            if(cRow>=0 && cRow<R && cCol>=0 && cCol<C) {
-                if (mat[cRow][cCol] > mat[row][col]) {
-                    max = Math.max(max, dfs(mat, dp, cRow, cCol, R, C)+1);
-                }
-            }
-        }
-        dp[row][col] = max;
+        int top = dfs(mat, dp, row-1, col, R, C, mat[row][col]);
+        int bottom = dfs(mat, dp, row+1, col, R, C, mat[row][col]);
+        int left = dfs(mat, dp, row, col-1, R, C, mat[row][col]);
+        int right = dfs(mat, dp, row, col+1, R, C, mat[row][col]);
+        dp[row][col] = Math.max(Math.max(top,bottom), Math.max(left, right)) + 1;
         return dp[row][col];
     }
+    
 }
