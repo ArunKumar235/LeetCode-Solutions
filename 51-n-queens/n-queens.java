14d63bbd36a1360628240f01810e99f3b88d2753
Arun Kumar M <marunkumar0203@gmail.com>
2024-09-28 11:36:23 +0530

    Time: 3 ms (56.40%) | Memory: 44.6 MB (89.36%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,54 +1,53 @@
 class Solution {
-    public boolean canPlace(boolean[]rowF,boolean[]colF,boolean[]trblF,boolean[]tlbrF,int row,int col,int n){
-        if(rowF[row] || colF[col] || trblF[row+col] || tlbrF[n-1-(row-col)]){
-            return false;
-        }
-        return true;
+    List<List<String>> result = new ArrayList<>();
+    boolean rowF[];
+    boolean colF[];
+    boolean trblF[];
+    boolean tlbrF[];
+    boolean board[][];
+    
+    public List<List<String>> solveNQueens(int n) {
+        rowF = new boolean[n];
+        colF = new boolean[n];
+        tlbrF = new boolean[2*n -1];
+        trblF = new boolean[2*n -1];
+        board = new boolean[n][n];
+        fill(0,n);
+        return result;
     }
-    public void fill(int row,boolean[][]board,boolean[]rowF,boolean[]colF,boolean[]trblF,boolean[]tlbrF,List<List<String>>ans,int n){
+    
+    public void fill(int row, int n){
         if(row==n){
-            List<String>placement=new ArrayList<>();
-            StringBuilder sb=new StringBuilder();
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(board[i][j]){
-                        sb.append("Q");
-                    }
-                    else{
-                        sb.append(".");
-                    }
+            // To do
+            List<String> li = new ArrayList<>();
+            for(int r = 0; r <n; r++){
+                StringBuilder sb = new StringBuilder();
+                for(int c = 0; c<n; c++){
+                    sb.append((board[r][c]) ? "Q" : ".");
                 }
-                placement.add(sb.toString());
-                sb.setLength(0);
+                li.add(sb.toString());
             }
-            ans.add(placement);
+            result.add(li);
             return;
         }
-        for(int col=0;col<n;col++){
-            if(canPlace(rowF,colF,trblF,tlbrF,row,col,n)){
-                rowF[row]=true;
-                colF[col]=true;
-                trblF[row+col]=true;
-                tlbrF[n-1-(row-col)]=true;
-                board[row][col]=true;
-                fill(row+1,board,rowF,colF,trblF,tlbrF,ans,n);
-                rowF[row]=false;
-                colF[col]=false;
-                trblF[row+col]=false;
-                tlbrF[n-1-(row-col)]=false;
-                board[row][col]=false;
-            }
-        }
 
+        for(int col = 0; col<n; col++){
+            if(!canPlace(row,col,n)) continue;
+            rowF[row] = true;
+            colF[col] = true;
+            trblF[row+col] = true;
+            tlbrF[n-1-(row-col)] = true;
+            board[row][col] = true;
+            fill(row+1, n);
+            rowF[row] = false;
+            colF[col] = false;
+            trblF[row+col] = false;
+            tlbrF[n-1-(row-col)] = false;
+            board[row][col] = false;
+        }
     }
-    public List<List<String>> solveNQueens(int n) {
-        boolean[][]board=new boolean[n][n];
-        boolean[]rowF=new boolean[n];
-        boolean[]colF=new boolean[n];
-        boolean[]trblF=new boolean[2*n - 1];
-        boolean[]tlbrF=new boolean[2*n - 1];
-        List<List<String>>ans=new ArrayList<>();
-        fill(0,board,rowF,colF,trblF,tlbrF,ans,n);
-        return ans;
+
+    public boolean canPlace(int row, int col, int n){
+        return !(rowF[row] || colF[col] || trblF[row+col] || tlbrF[n-1-(row-col)] || board[row][col]);
     }
 }
