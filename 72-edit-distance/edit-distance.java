23c7d482b1203131fd1c067411bfb2f4c8d2500b
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-01 11:44:58 +0530

    Time: 3 ms (96.49%) | Memory: 45 MB (49.89%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,22 +2,23 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i = 0; i<=n; i++){
-            dp[i][0] = i;
+        int[][] dp = new int[n][m];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        for(int j = 0; j<=m; j++){
-            dp[0][j] = j;
+        return solve(word1,word2,n-1,m-1,dp);
+    }
+    public int solve(String word1, String word2, int i, int j, int[][] dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            return dp[i][j] = solve(word1, word2, i-1,j-1,dp);
         }
-        for(int i =1 ;i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
-                }
-            }
-        }
-        return dp[n][m];
+        return dp[i][j] = 1 + Math.min(solve(word1, word2,i-1,j-1,dp), 
+            Math.min(solve(word1, word2,i-1,j,dp), 
+                    solve(word1, word2,i,j-1,dp)
+            )
+        );
     }
 }
