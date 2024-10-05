f74b934fddd2927ac7516727f12fb096627221b9
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-06 05:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (25.79%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,46 +1,49 @@
 class Solution {
-    private boolean isValid(int[][] grid, int row, int col) {
-        if (row >= grid.length || row < 0 || col >= grid[0].length || col < 0) return false;
-        if (grid[row][col] == -1) return false;
-        return true;
-    }
 
-    private int getUniquePaths(int[][] grid, int[][] diff, int row, int col, int step) {
-        if (grid[row][col] == 2) {
-            if (step == -1) return 1;
-            return 0;
-        }
-
-        int count = 0;
-        grid[row][col] = -1; // assigning to the current standing as if I mark next then I will never reach the goal(2) as it will be turned into (-1).
-        for (int[] choice : diff) {
-            int newRow = row + choice[0];
-            int newCol = col + choice[1];
-            if (isValid(grid, newRow, newCol)) {
-                count += getUniquePaths(grid, diff, newRow, newCol, step - 1);
-            }
-        }
-        grid[row][col] = 0;
-
-        return count;
-    }
+    private int m;
+    private int n;
+    private int paths = 0;
+    private boolean[][] visited;
+    private int totalEmpty = 0;
+    private int visitedEmpty = 0;
 
     public int uniquePathsIII(int[][] grid) {
-        int[][] diff = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
-
-        int sr = 0;
-        int sc = 0;
-        int steps = 0;
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        int startRow = -1;
+        int startColumn = -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    sr = i;
-                    sc = j;
-                } else if (grid[i][j] == 0) steps++;
+                    startRow = i;
+                    startColumn = j;
+                } else if (grid[i][j] == 0) {
+                    totalEmpty++;
+                }
             }
         }
-        
-        return getUniquePaths(grid, diff, sr, sc, steps);
+        uniquePathsIII(grid, startRow, startColumn);
+        return paths;
     }
+
+    private void uniquePathsIII(int[][] grid, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c]) return;
+        int value = grid[r][c];
+        if (value == -1) return;
+
+        visited[r][c] = true;
+        if (value == 0 || value == 1) {
+            if (value == 0) visitedEmpty++;
+            uniquePathsIII(grid, r + 1, c);
+            uniquePathsIII(grid, r, c + 1);
+            uniquePathsIII(grid, r - 1, c);
+            uniquePathsIII(grid, r, c - 1);
+        } else if (value == 2 && visitedEmpty == totalEmpty) {
+            paths++;
+        }
+        visited[r][c] = false;
+        if (value == 0) visitedEmpty--;
+    }
+
 }
