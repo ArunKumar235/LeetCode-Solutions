d714c28995f4521934c725ea8aa32e2fdf6cc775
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-13 16:06:47 +0530

    Time: 10 ms (74.50%) | Memory: 42 MB (86.58%) - LeetSync

--- 1343-dice-roll-simulation/dice-roll-simulation.java
+++ 1343-dice-roll-simulation/dice-roll-simulation.java
@@ -6,7 +6,7 @@ class Solution {
         sum[0] = 1;
         for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < 6; ++j)  {
-                for (int k = 1; k <= rollMax[j] && i - k >= 0; ++k)
+                for (int k = 1; k <= rollMax[j] && k<=i; ++k)
                     dp[i][j] = (dp[i][j] + sum[i-k] - dp[i-k][j] + p) % p;
                 sum[i] = (sum[i] + dp[i][j]) % p;
             }                
