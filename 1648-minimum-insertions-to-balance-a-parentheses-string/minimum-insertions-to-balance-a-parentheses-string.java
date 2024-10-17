3caf11077f447d28c71cfd513f28c0504bb3002f
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-17 08:24:40 +0530

    Time: 10 ms (82.93%) | Memory: 45.2 MB (73.05%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minInsertions(String s) {
-        int count=0;
-        int open=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch==')'){
+        int open = 0;
+        int count = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)==')'){
                 if(i+1<s.length() && s.charAt(i+1)==')'){
-                    if(open!=0)open--;
+                    if(open!=0) open--;
                     else count++;
                     i++;
                 }else{
                     if(open!=0){
                         count++;
                         open--;
-                    }else count+=2;
+                    }else{
+                        count+=2;
+                    }
                 }
-            }else open++;
-            
+            }else{
+                open++;
+            }
         }
-        return open*2+count;
+        return open*2 + count;
     }
 }
