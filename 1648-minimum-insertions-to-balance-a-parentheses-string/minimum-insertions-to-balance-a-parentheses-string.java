5ed9ce1fef4557e5b98ff6b390c1825ef973637c
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-17 08:28:53 +0530

    Time: 11 ms (70.03%) | Memory: 44.8 MB (99.42%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minInsertions(String s) {
-        int open = 0;
         int count = 0;
+        int open = 0;
         for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i)==')'){
-                if(i+1<s.length() && s.charAt(i+1)==')'){
-                    if(open!=0) open--;
-                    else count++;
-                    i++;
-                }else{
+            if(s.charAt(i)=='('){
+                open++;
+            }else{
+                if(i+1 < s.length() && s.charAt(i+1)==')'){
                     if(open!=0){
-                        count++;
                         open--;
                     }else{
+                        count++;
+                    }
+                    i++;
+                }else{
+                    if(open==0){
                         count+=2;
+                    }else{
+                        open--;
+                        count++;
                     }
                 }
-            }else{
-                open++;
             }
         }
         return open*2 + count;
