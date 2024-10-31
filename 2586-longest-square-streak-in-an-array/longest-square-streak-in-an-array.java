eeab614dc33bc121e6ea6a9b0b059bdd58870d24
Arun Kumar M <marunkumar0203@gmail.com>
2024-10-31 13:26:21 +0530

    Time: 57 ms (30.00%) | Memory: 58.3 MB (78.85%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -5,9 +5,13 @@ class Solution {
         int res = -1;
         for(int i = nums.length-1; i>=0; i--){
             int val = nums[i]*nums[i];
-            map.put(nums[i], map.getOrDefault(val, 0)+1);
-            res = Math.max(map.get(nums[i]), res);
+            if(map.containsKey(val)){
+                map.put(nums[i], map.get(val)+1);
+                res = Math.max(res, map.get(nums[i]));
+            }else{
+                map.put(nums[i], 1);
+            }
         }
-        return res==1 ? -1 : res;
+        return res;
     }
 }
