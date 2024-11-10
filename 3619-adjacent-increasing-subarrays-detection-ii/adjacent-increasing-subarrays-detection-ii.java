83a8e05da4f7d062860103ec4bc4fe32b65a9f20
Arun Kumar M <marunkumar0203@gmail.com>
2024-11-10 10:40:35 +0530

    Time: 90 ms (100.00%) | Memory: 74.4 MB (66.67%) - LeetSync

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int[] inc = new int[n];
+        
+        inc[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            inc[i] = nums.get(i) < nums.get(i + 1) ? inc[i + 1] + 1 : 1;
+        }
+        System.out.println(Arrays.toString(inc));
+        int maxK = 0;
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            int val = inc[i];
+            max = Math.max(max, val);
+            if(i+val < n && inc[i+val]>=inc[i]) maxK = Math.max(maxK, inc[i]);
+        }
+        return Math.max(max/2, maxK);
+    }
+}
