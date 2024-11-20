3c9e0929587b68948d3d122b434620827d6208ae
Arun Kumar M <marunkumar0203@gmail.com>
2024-11-20 18:05:40 +0530

    Time: 13 ms (74.55%) | Memory: 45.2 MB (75.15%) - LeetSync

--- 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
@@ -7,7 +7,8 @@ class Solution {
             totalCount[c - 'a']++;
         }
         for (int i = 0; i < 3; i++) {
-            if (totalCount[i] < k) return -1;
+            totalCount[i] -= k;
+            if (totalCount[i] < 0) return -1;
         }
 
         int maxLength = 0;
@@ -17,7 +18,7 @@ class Solution {
             windowCount[s.charAt(right) - 'a']++;
 
             while (left <= right && 
-                windowCount[s.charAt(right) - 'a'] > totalCount[s.charAt(right) - 'a']-k) {
+                windowCount[s.charAt(right) - 'a'] > totalCount[s.charAt(right) - 'a']) {
                 
                 windowCount[s.charAt(left) - 'a']--;
                 left++;
