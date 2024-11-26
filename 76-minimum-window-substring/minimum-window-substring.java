526c824e5602bfea91970c70e67e339055dfe291
Arun Kumar M <marunkumar0203@gmail.com>
2024-11-26 13:40:34 +0530

    Time: 17 ms (63.68%) | Memory: 44.8 MB (68.72%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,48 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
-            return "";
-        }
-        
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        
-        int count = map.size();
-        int left = 0, right = 0;
-        int minLength = Integer.MAX_VALUE;
-        int minLeft = 0;
+        if(s.isEmpty() || t.isEmpty() || s.length()<t.length()) return "";
 
-        while (right < s.length()) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i<t.length(); i++){
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i), 0)+1);
+        }
+        int count = map.size();
+        int left = 0;
+        int right = 0;
+        int minLeft = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while(right<s.length()){
             char toAdd = s.charAt(right);
-            if (map.containsKey(toAdd)) {
-                map.put(toAdd, map.get(toAdd) - 1);
-                if (map.get(toAdd) == 0) {
-                    count--;
-                }
-            }
-            
-            while (count == 0) {
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
+            if(map.containsKey(toAdd)) map.replace(toAdd, map.get(toAdd)-1);
+            if(map.getOrDefault(toAdd, 1)==0) count--;
+            while(count==0){
+                if(right-left+1 < minLength){
+                    minLength = right-left+1;
                     minLeft = left;
                 }
-                
                 char toRemove = s.charAt(left);
-                if (map.containsKey(toRemove)) {
-                    map.put(toRemove, map.get(toRemove) + 1);
-                    if (map.get(toRemove) > 0) {
-                        count++;
-                    }
+                if(map.containsKey(toRemove)){
+                    map.replace(toRemove, map.get(toRemove)+1);
+                    if(map.get(toRemove)==1) count++;
                 }
                 left++;
             }
-            
             right++;
         }
 
-        return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLength);
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft+minLength);
     }
 }
