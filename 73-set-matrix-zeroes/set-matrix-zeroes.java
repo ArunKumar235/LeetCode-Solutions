7bad528cda3813c73159820458b8750ef18bf7e8
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-04 21:40:48 +0530

    Time: 1 ms (76.26%) | Memory: 45.4 MB (71.91%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,23 +1,28 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        
-        int v[] = new int[r];
-        int h[] = new int[c];
-
-        for(int i = 0; i<r ; i++){
-            for(int j = 0; j<c ; j++){
-                if(matrix[i][j]==0){
-                    v[i] = -1;
-                    h[j] = -1;
+    public void setZeroes(int[][] mat) {
+        int r = mat.length;
+        int c = mat[0].length;
+        int row[] = new int[r];
+        int col[] = new int[c];
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(mat[i][j]==0){
+                    row[i] = -1;
+                    col[j] = -1;
                 }
             }
         }
-        for(int i = 0; i<r ; i++){
-            for(int j = 0; j<c ; j++){
-                if(v[i]==-1 || h[j] == -1){
-                    matrix[i][j]=0;
+        for(int i = 0; i<r; i++){
+            if(row[i]==-1){
+                for(int j = 0; j<c; j++){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        for(int j = 0; j<c; j++){
+            if(col[j]==-1){
+                for(int i = 0; i<r; i++){
+                    mat[i][j] = 0;
                 }
             }
         }
