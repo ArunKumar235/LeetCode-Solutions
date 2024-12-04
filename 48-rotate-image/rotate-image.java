805e3c278589289311df915e1c5935aa8fa3c20d
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-04 21:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (5.70%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,7 +1,7 @@
 class Solution {
     private void transpose(int[][] matrix) {
         for(int i=0; i<matrix.length; i++) {
-            for(int j=i; j<matrix[0].length; j++) {
+            for(int j=0; j<i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
