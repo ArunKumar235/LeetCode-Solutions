77b5a38192847d918de76913c401d298eab88050
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-09 18:33:40 +0530

    Time: 1897 ms (100.00%) | Memory: 44.8 MB (100.00%) - LeetSync

---
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    int res = Integer.MAX_VALUE;
+    public int findMinimumTime(List<Integer> strength, int K) {
+        boolean[] visited = new boolean[strength.size()];
+        permutate(visited, strength, new ArrayList<>(), K);
+        return res;
+    }
+    
+    public void permutate(boolean[] visited, List<Integer> s, List<Integer> curr, int k) {
+        if(curr.size() == s.size()){
+            res = Math.min(res, calc(curr, k));
+            return;
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            if(!visited[i]){
+                visited[i] = true;
+                curr.add(s.get(i));
+                permutate(visited, s, curr, k);
+                curr.remove(curr.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+    public int calc(List<Integer> curr, int k){
+        int x = 1;
+        int time = 0;
+
+        for(int s : curr){
+            time += Math.ceil(s/(1.0*x));
+            x += k;
+        }
+        return time;
+    }
+}
