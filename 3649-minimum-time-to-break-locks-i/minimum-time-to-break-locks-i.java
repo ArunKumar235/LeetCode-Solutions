4f7ed84a8a1f965fd6fba467bc3dd6cb6671eb7e
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-09 18:54:58 +0530

    Time: 1434 ms (100.00%) | Memory: 45.4 MB (100.00%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.java
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.java
@@ -1,35 +1,35 @@
 class Solution {
     int res = Integer.MAX_VALUE;
+    
     public int findMinimumTime(List<Integer> strength, int K) {
-        boolean[] visited = new boolean[strength.size()];
-        permutate(visited, strength, new ArrayList<>(), K);
+        permutate(strength, new ArrayList<Integer>(), new boolean[strength.size()], K);
         return res;
     }
-    
-    public void permutate(boolean[] visited, List<Integer> s, List<Integer> curr, int k) {
-        if(curr.size() == s.size()){
-            res = Math.min(res, calc(curr, k));
+
+    public void permutate(List<Integer> strength, List<Integer> curr, boolean[] visited, int K){
+        if(curr.size() == strength.size()){
+            calc(curr, K);
             return;
         }
-
-        for(int i = 0; i < s.size(); i++){
+        for(int i = 0; i<strength.size(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                curr.add(s.get(i));
-                permutate(visited, s, curr, k);
-                curr.remove(curr.size() - 1);
+                curr.add(strength.get(i));
+                permutate(strength, curr, visited, K);
+                curr.remove(curr.size()-1);
                 visited[i] = false;
             }
         }
     }
-    public int calc(List<Integer> curr, int k){
+
+    public void calc(List<Integer> curr, int K){
         int x = 1;
         int time = 0;
-
-        for(int s : curr){
+        for(int s: curr){
             time += (s+x-1)/x;
-            x += k;
+            x+=K;
         }
-        return time;
+        res = Math.min(res, time);
+        return;
     }
 }
