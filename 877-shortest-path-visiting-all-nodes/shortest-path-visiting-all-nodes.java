e81979f8c9963d95b3a1f555a58eb0259edcea8f
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-11 13:04:19 +0530

    Time: 95 ms (13.31%) | Memory: 51.9 MB (17.61%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
@@ -1,47 +1,40 @@
 class Solution {
     public static int shortestPathLength(int[][] graph) {
         int n = graph.length;
-        int req = (1 << n) - 1; // All nodes visited
-        Set<String> set = new HashSet<>();
-        Queue<Node> q = new LinkedList<>();
+        Set<String> set = new HashSet<>(); // track visited paths
+        Queue<Node> q = new LinkedList<>(); // store current level
+        int req = (1<<n) -1; // all nodes visited
 
-        // Initialize queue and visited set with all nodes
-        for (int i = 0; i < n; i++) {
-            int startTravel = 1 << i;
-            set.add(i + "_" + startTravel);
-            q.add(new Node(i, startTravel));
+        // initialize all the current nodes
+        for(int i = 0; i<n; i++){
+            int newTravel = 1<<i;
+            set.add(i+"_"+newTravel);
+            q.add(new Node(i, newTravel));
         }
-
-        int res = 0; // Tracks the number of steps
-        while (!q.isEmpty()) {
+        // track steps
+        int res = 0;
+        while(!q.isEmpty()){
             int size = q.size();
+            for(int i = 0; i<size; i++){
+                Node curr = q.poll();
+                int val = curr.val;
+                int travel = curr.travel;
 
-            // Process all nodes at the current level
-            for (int i = 0; i < size; i++) {
-                Node node = q.poll();
-                int val = node.val;
-                int travel = node.travel;
-
-                // If all nodes are visited, return result
-                if (travel == req) {
+                if(travel==req){ // check if all nodes are visited
                     return res;
                 }
 
-                // Explore neighbors
-                for (int newNode : graph[val]) {
-                    int newTravel = travel | (1 << newNode);
-                    String state = newNode + "_" + newTravel;
-
-                    if (!set.contains(state)) {
-                        q.add(new Node(newNode, newTravel));
-                        set.add(state);
+                for(int newNode: graph[val]){
+                    int newTravel = travel | (1<<newNode);
+                    String newState = newNode + "_" + newTravel;
+                    if(!set.contains(newState)){ // check if a path is already visited
+                       q.add(new Node(newNode, newTravel));
+                       set.add(newState);
                     }
                 }
             }
-
             res++;
         }
-
         return res;
     }
 }
