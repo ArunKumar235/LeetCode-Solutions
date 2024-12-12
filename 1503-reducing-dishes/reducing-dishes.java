780a67dd8e31d8ab0a997ce35e32deb8ff709779
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-12 21:24:45 +0530

    Time: 2 ms (100.00%) | Memory: 41.3 MB (88.07%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,18 +1,21 @@
+
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
-        int n = satisfaction.length;
         Arrays.sort(satisfaction);
-        int[][] dp = new int[n+1][n+1];
-        int max = 0;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=i; j++){
-                dp[i][j] = dp[i-1][j-1] + satisfaction[i-1]*(j);
-                max = Math.max(max, dp[i][j]);
+        int best = 0;   
+        int prefixSum = 0;      
+        int cur = 0;        
+        
+        for (int i = satisfaction.length - 1; i >= 0; i--) {    
+            prefixSum += satisfaction[i];
+            
+            if(prefixSum<0){
+                break;
             }
-        }
-        // for(int[] li: dp){
-        //     System.out.println(Arrays.toString(li));
-        // }
-        return max;
+
+            cur += prefixSum;           
+        }       
+
+        return cur;                    
     }
 }
