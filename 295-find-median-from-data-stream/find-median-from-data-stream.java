2ed75bc23034feff4915b99f8115fb31789cac12
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-14 23:13:52 +0530

    Time: 97 ms (59.47%) | Memory: 63.8 MB (45.64%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,25 +1,41 @@
- class MedianFinder {
-    
-    PriorityQueue<Integer> max = new PriorityQueue(Comparator.reverseOrder());
-    PriorityQueue<Integer> min = new PriorityQueue();
+class MedianFinder {
+
+    PriorityQueue<Integer> left_max_heap;
+    PriorityQueue<Integer> right_min_heap;
 
     public MedianFinder() {
-        
+        left_max_heap = new PriorityQueue<>(Collections.reverseOrder());
+        right_min_heap = new PriorityQueue<>();
     }
     
     public void addNum(int num) {
-       min.offer(num);
-       max.offer(min.poll());
-       if(min.size() < max.size()){
-         min.offer(max.poll());
-       }
+
+        if(left_max_heap.size() == 0 || num <= left_max_heap.peek())
+            left_max_heap.add(num);
+        else if(num > left_max_heap.peek())
+            right_min_heap.add(num);
+        
+        while((left_max_heap.size() - right_min_heap.size()) > 1) {
+            right_min_heap.add(left_max_heap.poll());
+        }
+        
+        while(right_min_heap.size() - left_max_heap.size() > 0) {
+            left_max_heap.add(right_min_heap.poll());
+        }
+
     }
     
     public double findMedian() {
-       if(min.size() == max.size()){
-         return (min.peek() + max.peek()) / 2.0;
-       } else {
-         return min.peek();
-       } 
+        if((right_min_heap.size()+left_max_heap.size()) % 2 == 0)
+            return (left_max_heap.peek()+right_min_heap.peek())/2.0;
+        else
+            return left_max_heap.peek();
     }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
