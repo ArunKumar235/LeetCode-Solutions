16552ef88bbcf0baab65f00fa187a597ec948a2e
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-23 15:09:37 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (19.69%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,17 +4,15 @@ class Solution {
         int c = arr[0].length;
         int[][] dp = new int[r][c];
         for(int i = 0; i<r; i++){
-            Arrays.fill(dp[i], -1);
+            for(int j = 0; j<c; j++){
+                if(arr[i][j] == 1) dp[i][j] = 0;
+                else if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    if(i>0) dp[i][j] += dp[i-1][j];
+                    if(j>0) dp[i][j] += dp[i][j-1];
+                }
+            }
         }
-        return rec(r-1, c-1, arr, dp);
-    }
-
-    private int rec(int r, int c, int[][] arr, int[][] dp){
-        if(r>=0 && c>=0 && arr[r][c] == 1) return 0;
-        if(r==0 && c==0) return 1;
-        if(r<0 || c<0) return 0;
-        if(dp[r][c] != -1) return dp[r][c];
-
-        return dp[r][c] = rec(r-1, c, arr, dp) + rec(r, c-1, arr, dp);
+        return dp[r-1][c-1];
     }
 }
