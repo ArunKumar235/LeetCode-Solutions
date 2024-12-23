922e3212d4a96430a4218085e934ab38de47d4e3
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-23 15:01:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (19.69%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,33 +1,18 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int r = obstacleGrid.length;
-        int c = obstacleGrid[0].length;
-        boolean blocked = false;
-        for(int i = 0; i<r; i++){
-            if(obstacleGrid[i][0]==1){
-                blocked = true;
-            }
-            if(blocked) obstacleGrid[i][0]=1;
-        }
-        blocked = false;
-        for(int i = 0; i<c; i++){
-            if(obstacleGrid[0][i]==1){
-                blocked = true;
-            }
-            if(blocked) obstacleGrid[0][i]=1;
-        }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int r = arr.length;
+        int c = arr[0].length;
         int[][] dp = new int[r][c];
         for(int i = 0; i<r; i++){
-            Arrays.fill(dp[i], -1);
+            for(int j = 0; j<c; j++){
+                if(arr[i][j] == 1) dp[i][j] = 0;
+                else if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    if(i>0) dp[i][j] += dp[i-1][j];
+                    if(j>0) dp[i][j] += dp[i][j-1];
+                }
+            }
         }
-        return rec(obstacleGrid, r-1, c-1, dp);
-    }
-
-    private int rec(int[][] mat, int r, int c, int[][] dp){
-        if(dp[r][c] != -1) return dp[r][c];
-        if(mat[r][c]==1) return 0;
-        if(r==0 || c==0) return 1;
-
-        return dp[r][c] = rec(mat, r-1, c, dp) + rec(mat, r, c-1, dp);
+        return dp[r-1][c-1];
     }
 }
