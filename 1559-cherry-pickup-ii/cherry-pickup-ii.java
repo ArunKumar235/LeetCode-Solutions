67b653f42645fec1543f82d6dc44d10bb1086901
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-24 17:47:47 +0530

    Time: 27 ms (45.03%) | Memory: 46.2 MB (37.27%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,31 +2,36 @@ class Solution {
     public int cherryPickup(int[][] arr) {
         int R = arr.length;
         int C = arr[0].length;
+
         int[][][] dp = new int[R][C][C];
-        for(int i = 0; i<R; i++){
-            for(int j = 0; j<C; j++){
-                Arrays.fill(dp[i][j], -1);
+
+        // Base case - Possible positions at last row
+        for(int c1 = 0; c1<C; c1++){
+            for(int c2 = 0; c2<C; c2++){
+                if(c1==c2) dp[R-1][c1][c2] = arr[R-1][c1];
+                else dp[R-1][c1][c2] = arr[R-1][c1] +arr[R-1][c2]; 
             }
         }
-        return rec(arr, 0, 0, C-1, R, C, dp);
-    }
 
-    private int rec(int[][] arr, int r, int c1, int c2, int R, int C, int[][][] dp){
-        if(c1<0 || c2<0 || c1>=C || c2>=C ) return Integer.MIN_VALUE;
-        if(dp[r][c1][c2] != -1) return dp[r][c1][c2]; 
-        if(r==R-1){
-            if(c1==c2) return arr[r][c1];
-            else return arr[r][c1] + arr[r][c2];
-        }
-
-        int max = 0;
-        for(int i = -1; i<=1; i++){
-            for(int j = -1; j<=1; j++){
-                max = Math.max(max, rec(arr, r+1, c1+i, c2+j, R, C, dp));
+        // Tabulation
+        for(int r = R-2; r>=0; r--){
+            for(int c1 = 0; c1<C; c1++){
+                for(int c2 = 0; c2<C; c2++){
+                    int max = 0;
+                    for(int i = -1; i<=1; i++){
+                        for(int j = -1; j<=1; j++){
+                            if(c1+i >= 0 && c1+i < C && c2+j>=0 && c2+j<C){        
+                                max = Math.max(max, dp[r+1][c1+i][c2+j]);
+                            }
+                        }
+                    }
+                    if(c1!=c2) max += arr[r][c1]+arr[r][c2];
+                    else max += arr[r][c1];
+                     dp[r][c1][c2] = max;
+                }
             }
         }
-        if(c1!=c2) max += arr[r][c1]+arr[r][c2];
-        else max += arr[r][c1];
-        return dp[r][c1][c2] = max;
+
+        return dp[0][0][C-1];
     }
 }
