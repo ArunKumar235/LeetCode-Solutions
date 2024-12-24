291950c766e683be2085f1bb5ea8f99aaf68da70
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-24 21:17:53 +0530

    Time: 17 ms (42.45%) | Memory: 44.7 MB (83.96%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,33 +1,42 @@
 class Solution {
-    public int cherryPickup(int[][] grid) {
-        int N = grid.length;
+    public int cherryPickup(int[][] arr) {
+        int N = arr.length;
         int[][][] dp = new int[N][N][N];
+        
         for(int[][] m : dp){
             for(int[] row : m){
                 Arrays.fill(row, Integer.MIN_VALUE);
             }
         }
+        dp[0][0][0] = arr[0][0];
 
-        return Math.max(0, rec(grid, 0, 0, 0, N, dp));
-    }
+        for(int r1 = 0; r1<N; r1++){
+            for(int c1 = 0; c1<N; c1++){
+                for(int c2 = 0; c2<N; c2++){
 
-    private int rec(int[][] arr, int r1, int c1, int c2, int N, int[][][] dp){
-        int r2 = r1+c1 - c2;
+                    int r2 = r1+c1-c2;
 
-        if(c1>=N || c2>=N || r1>=N || r2>=N || arr[r1][c1] == -1 || arr[r2][c2] == -1) return Integer.MIN_VALUE;
-        if((r1==N-1 && c1==N-1) || (r2==N-1 && c2==N-1)) return arr[r1][c1];
-        if(dp[r1][c1][c2] != Integer.MIN_VALUE) return dp[r1][c1][c2];
+                    if(r2<0 || r2>=N || arr[r1][c1] == -1 || arr[r2][c2] == -1) continue;
 
-        int dd = rec(arr, r1+1, c1, c2, N, dp);
-        int dr = rec(arr, r1+1, c1, c2+1, N, dp);
-        int rd = rec(arr, r1, c1+1, c2, N, dp);
-        int rr = rec(arr, r1, c1+1, c2+1, N, dp);
+                    int cherry = Integer.MIN_VALUE;
+                    if (r1 > 0 && r2 > 0) cherry = Math.max(cherry, dp[r1 - 1][c1][c2]);
+                    if (r1 > 0 && c2 > 0) cherry = Math.max(cherry, dp[r1 - 1][c1][c2 - 1]);
+                    if (c1 > 0 && r2 > 0) cherry = Math.max(cherry, dp[r1][c1 - 1][c2]);
+                    if (c1 > 0 && c2 > 0) cherry = Math.max(cherry, dp[r1][c1 - 1][c2 - 1]);
 
-        int res = Math.max(dd, Math.max(dr, Math.max(rd, rr)));
+                    if (cherry == Integer.MIN_VALUE) {
+                        continue;
+                    }
 
-        if(r1==r2 && c1==c2) res += arr[r1][c1];
-        else res += arr[r1][c1] + arr[r2][c2];
+                    dp[r1][c1][c2] = cherry + arr[r1][c1];
 
-        return dp[r1][c1][c2] = res;
+                    if (r1 != r2 || c1 != c2) {
+                        dp[r1][c1][c2] += arr[r2][c2];
+                    }
+                }
+            }
+        }
+
+        return Math.max(0, dp[N-1][N-1][N-1]);
     }
 }
