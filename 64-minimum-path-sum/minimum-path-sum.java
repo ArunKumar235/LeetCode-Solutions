f35b116b3697ae7a9865d52abe46439518972dd6
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-24 11:57:48 +0530

    Time: 5 ms (31.59%) | Memory: 47.9 MB (25.09%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -4,13 +4,17 @@ class Solution {
         int c = arr[0].length;
 
         int[][] dp = new int[r][c];
-
-        for(int i = 0; i<r; i++){
-            for(int j = 0; j<c; j++){
+        dp[0][0] = arr[0][0];
+        for(int i = 1; i<r; i++){
+            dp[i][0] = dp[i-1][0] + arr[i][0];
+        }
+        for(int j = 1; j<c; j++){
+            dp[0][j] = dp[0][j-1] + arr[0][j];
+        }
+        for(int i = 1; i<r; i++){
+            for(int j = 1; j<c; j++){
                 dp[i][j] = arr[i][j];
-                if(i>0 && j>0) dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
-                else if(i>0) dp[i][j] += dp[i-1][j];
-                else if(j>0) dp[i][j] += dp[i][j-1];
+                dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
             }
         }
         return dp[r-1][c-1];
