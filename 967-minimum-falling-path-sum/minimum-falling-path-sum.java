9669b5193534f68f49963c393c99927f228a08fd
Arun Kumar M <marunkumar0203@gmail.com>
2024-12-24 13:31:06 +0530

    Time: 6 ms (66.90%) | Memory: 45.4 MB (49.72%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minFallingPathSum(int[][] arr) {
         int r = arr.length;
-        int[][] dp = new int[r][r];
-        
+        int[] prev = new int[r];
         for(int i = 0; i<r; i++){
+            int[] curr = new int[r];
             for(int j = 0; j<r; j++){
-                dp[i][j] = arr[i][j]; 
+                curr[j] = arr[i][j]; 
                 if(i==0) continue;
-                if(j>0 && j+1!=r) dp[i][j] += Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i-1][j+1]));
-                else if(j>0) dp[i][j] += Math.min(dp[i-1][j-1], dp[i-1][j]);
-                else if(j+1!=r) dp[i][j] += Math.min(dp[i-1][j], dp[i-1][j+1]);
+                if(j>0 && j+1!=r) curr[j] += Math.min(prev[j-1], Math.min(prev[j], prev[j+1]));
+                else if(j>0) curr[j] += Math.min(prev[j-1], prev[j]);
+                else if(j+1!=r) curr[j] += Math.min(prev[j], prev[j+1]);
             }
+            prev = curr;
         }
         int res = Integer.MAX_VALUE;
-        for(int v: dp[r-1]){
+        for(int v: prev){
             res = Math.min(res, v);
         }
         return res;
