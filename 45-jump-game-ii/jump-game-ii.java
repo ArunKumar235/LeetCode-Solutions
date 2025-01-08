e7380900abc780bf18dfac1cccdd6a20b606e9db
Arun Kumar M <marunkumar0203@gmail.com>
2025-01-08 11:02:35 +0530

    Time: 1 ms (99.29%) | Memory: 45.1 MB (56.63%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,20 +1,25 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        if(n==1) return 0;
 
-        return rec(nums, dp, 0);
-    }
+        int maxReachIdx = nums[0];
+        int jump = 1;
+        int steps = nums[0];
 
-    private int rec(int[] arr, int[] dp, int idx){
-        if(idx>=arr.length-1) return 0;
-        if(arr[idx]==0) return 10000; // safe max value
-        if(dp[idx] != Integer.MAX_VALUE) return dp[idx];
-        
-        for(int i = 1; i<=arr[idx]; i++){
-            dp[idx] = Math.min(dp[idx], 1 + rec(arr, dp, idx+i));
-        } 
-        return dp[idx];
+        for(int idx = 1; idx<n-1; idx++){
+            if(idx+nums[idx] > maxReachIdx){
+                maxReachIdx = idx+nums[idx];
+            }
+
+            steps--;
+
+            if(steps==0){
+                jump++;
+                steps = maxReachIdx - idx;
+            }
+        }
+
+        return jump;
     }
 }
