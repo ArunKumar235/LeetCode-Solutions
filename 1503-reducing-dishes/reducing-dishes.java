520a91b0342a61d100f919b99fcb9f736d850140
Arun Kumar M <marunkumar0203@gmail.com>
2025-01-26 22:17:02 +0530

    Time: 2 ms (100.00%) | Memory: 41.7 MB (74.55%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,21 +1,16 @@
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
-        Arrays.sort(satisfaction);
-        int n = satisfaction.length;
-        int[][] dp = new int[n+1][n+1];
-        for(int[] li: dp){
-            Arrays.fill(li, -1);
+    public int maxSatisfaction(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        
+        int pre = 0;
+        int res = 0;
+
+        for(int i = n-1; i>=0; i--){
+            pre+=arr[i];
+            if(pre<0) break;
+            res+=pre;
         }
-        return rec(satisfaction, 0, 1, dp);
-    }
-
-    private int rec(int[] arr, int idx, int mul, int[][] dp){
-        if(idx==arr.length) return 0;
-        if(dp[idx][mul] != -1) return dp[idx][mul];
-
-        int pick = arr[idx]*mul + rec(arr, idx+1, mul+1, dp);
-        int not_pick = rec(arr, idx+1, mul, dp);
-
-        return dp[idx][mul] = Math.max(pick, not_pick);
+        return res;
     }
 }
