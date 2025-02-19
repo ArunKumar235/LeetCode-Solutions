e24f928f40b63c2e31d5983965909ba5fe864335
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-19 19:19:19 +0530

    Time: 24 ms (74.77%) | Memory: 46.1 MB (44.55%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,36 +2,31 @@ class Solution {
     public int cherryPickup(int[][] arr) {
         int R = arr.length;
         int C = arr[0].length;
-
         int[][][] dp = new int[R][C][C];
-
-        // Base case - Possible positions at last row
-        for(int c1 = 0; c1<C; c1++){
-            for(int c2 = 0; c2<C; c2++){
-                if(c1==c2) dp[R-1][c1][c2] = arr[R-1][c1];
-                else dp[R-1][c1][c2] = arr[R-1][c1] +arr[R-1][c2]; 
+        for(int i = 0; i<R; i++){
+            for(int j = 0; j<C; j++){
+                Arrays.fill(dp[i][j], -1);
             }
         }
+        return rec(arr, 0, 0, C-1, dp, R, C);
+    }
 
-        // Tabulation
-        for(int r = R-2; r>=0; r--){
-            for(int c1 = 0; c1<C; c1++){
-                for(int c2 = 0; c2<C; c2++){
-                    int max = 0;
-                    for(int i = -1; i<=1; i++){
-                        for(int j = -1; j<=1; j++){
-                            if(c1+i >= 0 && c1+i < C && c2+j>=0 && c2+j<C){        
-                                max = Math.max(max, dp[r+1][c1+i][c2+j]);
-                            }
-                        }
-                    }
-                    if(c1!=c2) max += arr[r][c1]+arr[r][c2];
-                    else max += arr[r][c1];
-                     dp[r][c1][c2] = max;
-                }
+    private int rec(int[][] arr, int r, int c1, int c2, int[][][] dp, int R, int C){
+        if(c1<0 || c2<0 || c1>=C || c2>=C){
+            return Integer.MIN_VALUE;
+        }
+        if(r==R) return 0;
+        if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
+
+        int res = 0;
+
+        for(int i = -1; i<=1; i++){
+            for(int j = -1; j<=1; j++){
+                res = Math.max(res, rec(arr, r+1, c1+i, c2+j, dp, R, C));
             }
         }
+        res += (c1==c2) ? arr[r][c1] : arr[r][c1]+arr[r][c2];
 
-        return dp[0][0][C-1];
+        return dp[r][c1][c2]  = res;
     }
 }
