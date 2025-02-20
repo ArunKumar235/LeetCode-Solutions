c5d70ce2e6ba258ad4a812309edb38937005c01f
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-21 02:41:31 +0530

    Time: 43 ms (52.80%) | Memory: 57.3 MB (24.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int N = nums.length;
+        int tsum = 0;
+        for(int num: nums) tsum+= num;
+        if(tsum%2==1) return false;
+        int target = tsum/2;
 
-        int totalSum = 0;
-        for(int num: nums){
-            totalSum += num;
-        }
-
-        if(totalSum %2 == 1) return false;
-
-        int[][] dp = new int[N][totalSum/2 +1];
+        int[][] dp = new int[nums.length][target+1];
         for(int[] row: dp){
             Arrays.fill(row, -1);
-        }
+        }                
 
-        return rec(N-1, totalSum/2, nums, dp);
+        return rec(nums.length-1, target, nums, dp);
     }
 
     private boolean rec(int idx, int target, int[] nums, int[][] dp){
-        if(idx==0) return nums[0] == target;
+        if(idx<0) return target==0;
         if(target==0) return true;
-        if(dp[idx][target] != -1) return dp[idx][target] == 1;
+        if(target<0) return false;
+        if(dp[idx][target] != -1) return dp[idx][target]==1;
 
-        boolean notTake = rec(idx-1, target, nums, dp);
-        boolean take = false;
-        if(nums[idx] <= target) take = rec(idx-1, target - nums[idx], nums, dp);
+        dp[idx][target] = (rec(idx-1,target,nums,dp) || rec(idx-1,target-nums[idx],nums,dp)) ? 1 : 0;
 
-        dp[idx][target] = (take || notTake) ? 1 : 0;
-
-        return dp[idx][target] == 1;
+        return dp[idx][target]==1;
     }
 }
