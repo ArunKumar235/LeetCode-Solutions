36b434670fec3531e8a90aefa9cf5db7932a84e9
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-20 21:54:09 +0530

    Time: 15 ms (72.56%) | Memory: 45.5 MB (42.41%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,42 +1,39 @@
 class Solution {
-    public int cherryPickup(int[][] arr) {
-        int N = arr.length;
+    public int cherryPickup(int[][] grid) {
+        int N = grid.length;
         int[][][] dp = new int[N][N][N];
-        
-        for(int[][] m : dp){
-            for(int[] row : m){
+        for(int[][] mat: dp){
+            for(int[] row: mat){
                 Arrays.fill(row, Integer.MIN_VALUE);
             }
         }
-        dp[0][0][0] = arr[0][0];
 
-        for(int r1 = 0; r1<N; r1++){
-            for(int c1 = 0; c1<N; c1++){
-                for(int c2 = 0; c2<N; c2++){
+        int res = rec(0,0,0, grid, dp, N);
 
-                    int r2 = r1+c1-c2;
+        if(res<0) return 0;
 
-                    if(r2<0 || r2>=N || arr[r1][c1] == -1 || arr[r2][c2] == -1) continue;
+        return res;
+    }
 
-                    int cherry = Integer.MIN_VALUE;
-                    if (r1 > 0 && r2 > 0) cherry = Math.max(cherry, dp[r1 - 1][c1][c2]);
-                    if (r1 > 0 && c2 > 0) cherry = Math.max(cherry, dp[r1 - 1][c1][c2 - 1]);
-                    if (c1 > 0 && r2 > 0) cherry = Math.max(cherry, dp[r1][c1 - 1][c2]);
-                    if (c1 > 0 && c2 > 0) cherry = Math.max(cherry, dp[r1][c1 - 1][c2 - 1]);
+    private int rec(int r1, int c1, int c2, int[][] arr, int[][][] dp, int N){
+        int r2 = r1+c1-c2;
+        if(r1<0 || r2<0 || c1<0 || c2<0 || r1>=N || r2>=N || c1>=N || c2>=N ) return Integer.MIN_VALUE;
+        if(arr[r1][c1]==-1 || arr[r2][c2]==-1) return Integer.MIN_VALUE;
+        if(r1==N-1 && c1==N-1) return arr[r1][c1];
+        
+        if(dp[r1][c1][c2] != Integer.MIN_VALUE) return dp[r1][c1][c2];
 
-                    if (cherry == Integer.MIN_VALUE) {
-                        continue;
-                    }
+        int dd = rec(r1+1, c1, c2, arr, dp, N);
+        int dr = rec(r1+1, c1, c2+1, arr, dp, N);
+        int rd = rec(r1, c1+1, c2, arr, dp, N);
+        int rr = rec(r1, c1+1, c2+1, arr, dp, N);
 
-                    dp[r1][c1][c2] = cherry + arr[r1][c1];
+        int res = Math.max(dd, Math.max(dr, Math.max(rd, rr)));
 
-                    if (r1 != r2 || c1 != c2) {
-                        dp[r1][c1][c2] += arr[r2][c2];
-                    }
-                }
-            }
-        }
+        if(r1==r2 && c1==c2) res += arr[r1][c1];
+        else res += arr[r1][c1] + arr[r2][c2];
 
-        return Math.max(0, dp[N-1][N-1][N-1]);
+        return dp[r1][c1][c2] = res; 
+
     }
 }
