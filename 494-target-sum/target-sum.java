4eda63fc914e021eb6998ad485cbedc370b26df7
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-24 18:02:24 +0530

    Time: 5 ms (90.65%) | Memory: 43 MB (43.96%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -6,32 +6,26 @@ class Solution {
         int k = (total-target)/2;
 
         if((total-target)<0 || (total-target)%2==1) return 0;
+        
+        // count subsets with sum k
 
         int[][] dp = new int[nums.length][k+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        
-        return rec(nums.length-1, k, nums, dp);
-    }
-    // a+b = Tot
-    // a-b = tar
-    // Tot-b-b = tar
 
-    // Tot = tar+2b
-    // (Tot-tar)/2 = b
+        if(nums[0]==0) dp[0][0] = 2;
+        else dp[0][0] = 1;
 
-    // count subsets with sum k
-    private int rec(int idx, int target, int[] nums, int[][] dp){
-        if(idx==0){
-            if(nums[0]==0 && target==0) return 2;
-            if(nums[0]==target || target==0) return 1;
-            return 0;
+        if(nums[0]!=0 && nums[0]<=k) dp[0][nums[0]] = 1;
+
+        for(int i = 1; i<nums.length; i++){
+            for(int j = 0; j<=k; j++){
+                int nottake = dp[i-1][j];
+                int take = 0;
+                if(j-nums[i]>=0) take = dp[i-1][j-nums[i]];
+
+                dp[i][j] = nottake+take;
+            }
         }
-        if(dp[idx][target]!=-1) return dp[idx][target];
 
-        int notpick = rec(idx-1, target, nums, dp);
-        int pick = 0; 
-        if(target-nums[idx]>=0) pick = rec(idx-1, target-nums[idx], nums, dp);
-
-        return dp[idx][target] = pick + notpick;
+        return dp[nums.length-1][k];
     }
 }
