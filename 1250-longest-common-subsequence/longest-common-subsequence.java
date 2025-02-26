71ffdaac1c4ee3abf98665c95f7426173e8678cd
Arun Kumar M <marunkumar0203@gmail.com>
2025-02-26 17:49:53 +0530

    Time: 21 ms (55.75%) | Memory: 51.1 MB (42.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,23 +2,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int l1 = text1.length();
         int l2 = text2.length();
-        int[][] dp = new int[l1][l2];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return rec(l1-1, l2-1, text1, text2, dp);
-    }
+        int[][] dp = new int[l1+1][l2+1];
 
-    private int rec(int idx1, int idx2, String str1, String str2, int[][] dp){
-        if(idx1<0 || idx2<0){
-            return 0;
+        for(int i = 1; i<=l1; i++){
+            for(int j = 1; j<=l2; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        
-        if(str1.charAt(idx1)==str2.charAt(idx2)){
-            return dp[idx1][idx2] = 1 + rec(idx1-1, idx2-1, str1, str2, dp);
-        }
-        int move1 = rec(idx1-1, idx2, str1, str2, dp);
-        int move2 = rec(idx1, idx2-1, str1, str2, dp);
-
-        return dp[idx1][idx2] = Math.max(move1, move2);
+        return dp[l1][l2];
     }
 }
