a5771b7ad13ffd20b1756dbe75b4b569e76e516e
Arun Kumar M <marunkumar0203@gmail.com>
2025-03-02 18:19:53 +0530

    Time: 183 ms (28.10%) | Memory: 44.7 MB (56.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,10 +3,10 @@ class Solution {
         int N = s.length();
         int maxLen = 0;
         String res = "";
-        Boolean palin[][] = new Boolean[N][N];
         for(int i = 0; i<N; i++){
             for(int j = i; j<N; j++){
-                if(isPalin(s, i, j, palin)){
+                if(j-i+1<maxLen) continue;
+                if(isPalin(s, i, j)){
                     if(j-i+1>maxLen){
                         maxLen = j-i+1;
                         res = s.substring(i, j+1);
@@ -17,13 +17,12 @@ class Solution {
         return res;
     }
 
-    private boolean isPalin(String str, int start, int end, Boolean[][] palin){
+    private boolean isPalin(String str, int start, int end){
         while(start<end){
-            if(str.charAt(start)!=str.charAt(end)) return palin[start][end] = false;
-            if(palin[start][end]!=null) return palin[start][end];
+            if(str.charAt(start)!=str.charAt(end)) return false;
             start++;
             end--;
         }
-        return palin[start][end] = true;
+        return true;
     }
 }
