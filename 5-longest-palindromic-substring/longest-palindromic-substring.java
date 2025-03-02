13f7557a57916a593176d7eda36aba1c64254fe4
Arun Kumar M <marunkumar0203@gmail.com>
2025-03-02 18:16:25 +0530

    Time: 1672 ms (10.26%) | Memory: 44.6 MB (57.26%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,30 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int resLen = 0;
+        int N = s.length();
+        int maxLen = 0;
         String res = "";
-        int len = s.length();
-        for(int i = 0; i<len; i++){
-            int l = i;
-            int r = i;
-            while ( l>=0 && r<len && s.charAt(l)==s.charAt(r)){
-                if(resLen < r-l+1){
-                    res = s.substring(l,r+1);
-                    resLen = r-l+1;
+        for(int i = 0; i<N; i++){
+            for(int j = i; j<N; j++){
+                if(isPalin(s, i, j)){
+                    if(j-i+1>maxLen){
+                        maxLen = j-i+1;
+                        res = s.substring(i, j+1);
+                    }
                 }
-                l-=1;
-                r+=1;
-            }
-            l = i;
-            r = i+1;
-            while ( l>=0 && r<len && s.charAt(l)==s.charAt(r)){
-                if(resLen < r-l+1){
-                    res = s.substring(l,r+1);
-                    resLen = r-l+1;
-                }
-                l-=1;
-                r+=1;
             }
         }
         return res;
+    }
+
+    private boolean isPalin(String str, int start, int end){
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 }
