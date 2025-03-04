3686365e5567d3934138697977d782b41b31dfc7
Arun Kumar M <marunkumar0203@gmail.com>
2025-03-04 08:56:46 +0530

    Time: 227 ms (29.60%) | Memory: 80.3 MB (6.25%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.java
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.java
@@ -1,27 +1,21 @@
 class Solution {
     public long maxSum(int[][] grid, int[] limits, int k) {
-
-        PriorityQueue<Integer> res = new PriorityQueue<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
 
         for(int i = 0; i<grid.length; i++){
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
             for(int j = 0; j<grid[i].length; j++){
-                if(pq.size() < limits[i]) pq.add(grid[i][j]);
-                else{
-                    pq.add(grid[i][j]);
-                    pq.poll();
-                }
-            }
-            for(int val: pq){
-                if(res.size() < k) res.add(val);
-                else{
-                    res.add(val);
-                    res.poll();
-                }
+                pq.add(new int[]{grid[i][j], i});
             }
         }
-        long ans = 0;
-        for(int val: res) ans += val;
-        return ans;
+        long sum = 0;
+        while(k!=0){
+            int[] arr = pq.poll();
+            if(limits[arr[1]]!=0){
+                sum+=arr[0];
+                limits[arr[1]]--;
+                k--;
+            }
+        }
+        return sum;
     }
 }
