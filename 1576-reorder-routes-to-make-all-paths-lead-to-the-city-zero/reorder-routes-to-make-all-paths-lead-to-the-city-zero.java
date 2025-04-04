aeae1fbe15de0d81da9b66e67e2b2c709e63eb6c
Arun Kumar M <marunkumar0203@gmail.com>
2025-04-04 09:01:12 +0530

    Time: 88 ms (37.28%) | Memory: 76.7 MB (33.78%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minReorder(int n, int[][] connections) {
+        Map<Integer, List<Edge>> adj = new HashMap<>();
+
+        for(int[] arr: connections){
+            adj.computeIfAbsent(arr[0], k -> new ArrayList<Edge>()).add(new Edge(arr[1], 1));
+            adj.computeIfAbsent(arr[1], k -> new ArrayList<Edge>()).add(new Edge(arr[0], 0));
+        }
+        return bfs(0, n, adj);
+    }
+
+    private int bfs(int node, int n, Map<Integer, List<Edge>> adj){
+        int count = 0;
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visited[node] = true;
+
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(!adj.containsKey(curr)){
+                continue;
+            }
+            for(Edge e: adj.get(curr)){
+                if(!visited[e.node]){
+                    visited[e.node] = true;
+                    q.offer(e.node);
+                    count += e.weight;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static class Edge{
+        int node;
+        int weight;
+
+        Edge(int node, int weight){
+            this.node = node;
+            this.weight = weight;
+        }
+    }
+}
