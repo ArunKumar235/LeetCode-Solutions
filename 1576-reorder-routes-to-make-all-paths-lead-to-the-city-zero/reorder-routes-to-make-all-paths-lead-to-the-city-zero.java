67e478d75a3f3f35126299f8db072798cab2957c
Arun Kumar M <marunkumar0203@gmail.com>
2025-04-04 09:02:57 +0530

    Time: 67 ms (49.64%) | Memory: 76.2 MB (37.08%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -32,14 +32,14 @@ class Solution {
 
         return count;
     }
+}
 
-    static class Edge{
-        int node;
-        int weight;
+class Edge{
+    int node;
+    int weight;
 
-        Edge(int node, int weight){
-            this.node = node;
-            this.weight = weight;
-        }
+    Edge(int node, int weight){
+        this.node = node;
+        this.weight = weight;
     }
 }
