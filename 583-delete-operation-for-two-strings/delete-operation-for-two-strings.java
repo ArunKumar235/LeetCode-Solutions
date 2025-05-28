00a116fc565516dc6cc2fc1874da6d9b4e606058
Arun Kumar M <marunkumar0203@gmail.com>
2025-05-28 13:41:38 +0530

    Time: 7 ms (73.01%) | Memory: 44.5 MB (92.31%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,19 +1,23 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int l1 = word1.length();
-        int l2 = word2.length();
-        int[][] dp = new int[l1+1][l2+1];
-        for(int i = 1; i<=l1; i++){
-            for(int j = 1; j<=l2; j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)){
+        int LCS = lcs(word1, word2);
+        int deletions1 = word1.length() - LCS;
+        int deletions2 = word2.length() - LCS;
+
+        return deletions1 + deletions2;
+    }
+
+    public int lcs(String str1, String str2){
+        int[][] dp = new int[str1.length()+1][str2.length()+1];
+        for(int i = 1; i<=str1.length(); i++){
+            for(int j = 1; j<=str2.length(); j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        int lcs = dp[l1][l2];
-
-        return l1+l2-2*lcs;
+        return dp[str1.length()][str2.length()];
     }
 }
