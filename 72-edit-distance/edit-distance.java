3db289f7ad1be2b4f1857124ba19708450a374a8
Arun Kumar M <marunkumar0203@gmail.com>
2025-05-29 13:51:11 +0530

    Time: 5 ms (62.57%) | Memory: 45 MB (63.05%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,25 +3,23 @@ class Solution {
         int l1 = a.length();
         int l2 = b.length();
 
-        int[][] dp = new int[l1+1][l2+1];
+        int[] prev = new int[l2+1]; 
+        int[] curr = new int[l2+1];
 
-        for(int i = 0; i<=l1; i++){
-            dp[i][0] = i;
-        }
-        for(int j = 0; j<=l2; j++){
-            dp[0][j] = j;
-        }
+        for(int i = 0; i<=l2; i++) prev[i] = i;
 
         for(int i = 1; i<=l1; i++){
+            curr[0] = i;
             for(int j = 1; j<=l2; j++){
-                if(a.charAt(i-1)!=b.charAt(j-1)){
-                    dp[i][j] = 1 + Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    curr[j] = prev[j-1];
                 }else{
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = 1 + Math.min(curr[j-1], Math.min(prev[j], prev[j-1]));
                 }
             }
+            prev = curr.clone();
         }
-        return dp[l1][l2];
 
+        return prev[l2];
     }
 }
