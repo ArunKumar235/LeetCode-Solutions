116bf2695603de1bc712c9800104fbc49ec8de3b
Arun Kumar M <marunkumar0203@gmail.com>
2025-05-29 14:57:32 +0530

    Time: 16 ms (76.55%) | Memory: 48.7 MB (22.65%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,32 +1,34 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m=s.length();
-        int n=p.length();
-        boolean dp[][]=new boolean[n+1][m+1];
-        dp[0][0]=true;
-        
-        
-        // to detect continous star
-       for(int i=1;i<=n;i++){
-            if(p.charAt(i-1)=='*'){
-                dp[i][0] = dp[i-1][0];
-            }
+        int l1 = s.length();
+        int l2 = p.length();
+
+        int[][] dp = new int[l1][l2];
+        for(int[] arr: dp){
+            Arrays.fill(arr, -1);
         }
 
+        return func(s, p, l1-1, l2-1, dp) == 1;
+    }
 
-        // tabulation 
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){ 
-                if(s.charAt(j-1)==p.charAt(i-1)|| p.charAt(i-1)=='?'){
-                    dp[i][j]=dp[i-1][j-1];
-                    
-                }else if(p.charAt(i-1)=='*'){
-                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                }else{
-                    dp[i][j]=false;
-                }       
-            }
+    private int func(String s, String p, int i, int j, int[][] dp){
+        if(i<0 && j<0) return 1;
+
+        if(j<0 && i>=0) return 0;
+
+        if(i<0 && j>=0){ // when s is over and p is not over, remaining p must be of *
+            for(int k = 0; k<=j; k++) if(p.charAt(k) != '*') return 0;
+            return 1;
         }
-       return dp[n][m]; 
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?') return dp[i][j] = func(s, p, i-1, j-1, dp);
+        
+        if(p.charAt(j)=='*') 
+            return dp[i][j] = (func(s, p, i-1, j, dp)==1  // char covered
+                || func(s, p, i, j-1, dp)==1) ? 1 : 0 ; // char not covered
+        
+        return dp[i][j] = 0;
     }
 }
