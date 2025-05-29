cc0d63d32c47446371e5f92de2a8541dd9031222
Arun Kumar M <marunkumar0203@gmail.com>
2025-05-29 13:25:35 +0530

    Time: 5 ms (62.57%) | Memory: 45.1 MB (46.33%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,11 +1,28 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int l1 = word1.length();
-        int l2 = word2.length();
+    public int minDistance(String a, String b) {
+        int l1 = a.length();
+        int l2 = b.length();
 
-        int[][] dp = new int[l1][l2];
+        int[][] dp = new int[l1+1][l2+1];
 
-        return func(word1, word2, l1-1, l2-1, dp);
+        for(int i = 0; i<=l1; i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0; j<=l2; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i = 1; i<=l1; i++){
+            for(int j = 1; j<=l2; j++){
+                if(a.charAt(i-1)!=b.charAt(j-1)){
+                    dp[i][j] = 1 + Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+                }else{
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[l1][l2];
+
     }
 
     private int func(String a, String b, int i, int j, int[][] dp){
