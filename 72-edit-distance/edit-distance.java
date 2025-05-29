f217e9b48b62027a736bfc3cae3a3b35a3627517
Arun Kumar M <marunkumar0203@gmail.com>
2025-05-29 13:18:58 +0530

    Time: 2 ms (100.00%) | Memory: 45.2 MB (46.33%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-        return solve(word1,word2,n-1,m-1,dp);
+        int l1 = word1.length();
+        int l2 = word2.length();
+
+        int[][] dp = new int[l1][l2];
+
+        return func(word1, word2, l1-1, l2-1, dp);
     }
-    public int solve(String word1, String word2, int i, int j, int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j)){
-            return dp[i][j] = solve(word1, word2, i-1,j-1,dp);
-        }
-        return dp[i][j] = 1 + Math.min(solve(word1, word2,i-1,j-1,dp), 
-            Math.min(solve(word1, word2,i-1,j,dp), 
-                    solve(word1, word2,i,j-1,dp)
+
+    private int func(String a, String b, int i, int j, int[][] dp){
+        if(i<0) return j+1; // insertions
+        if(j<0) return i+1; // deletions
+
+        if(dp[i][j] !=0 ) return dp[i][j];
+        
+        if(a.charAt(i)==b.charAt(j)) return dp[i][j] = func(a, b, i-1, j-1, dp);
+
+        else return dp[i][j] = 1 + Math.min( 
+            func(a, b, i, j-1, dp), // insert
+            Math.min( func(a, b, i-1, j, dp), // delete
+                func(a, b, i-1, j-1, dp) // replace
             )
         );
     }
