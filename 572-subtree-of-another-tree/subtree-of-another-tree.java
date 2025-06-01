614c1ca0f51526bee8aa4b3ef20014b2a886a29f
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-01 08:09:55 +0530

    Time: 3 ms (23.45%) | Memory: 44.7 MB (21.61%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,24 +15,27 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null) return true;
-        if (root == null) return false;
-        
-        StringBuilder text = new StringBuilder();
-        StringBuilder pattern = new StringBuilder();
-        
-        dfs(root, text);
-        dfs(subRoot, pattern);
-        return text.toString().contains(pattern.toString());
+        return search(root, subRoot);
     }
 
-    private void dfs(TreeNode node, StringBuilder sb) {
-        if (node == null) {
-            sb.append("null,");
-            return;
+    private boolean search(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null || subRoot==null) return false;
+
+        if(root.val==subRoot.val){
+            if(isSameTree(root, subRoot)){
+                return true;
+            }
         }
-        sb.append("#").append(node.val).append(",");
-        dfs(node.left, sb);
-        dfs(node.right, sb);
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+
+        if(p.val!=q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
