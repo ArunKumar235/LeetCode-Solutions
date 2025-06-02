60ec147c3aa748379259dcaaf258671bde44b3b3
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-02 14:39:10 +0530

    Time: 1 ms (78.87%) | Memory: 45.8 MB (58.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] arr: dp){
-            Arrays.fill(arr, Integer.MIN_VALUE);
-        }
-        return func(0, 0, prices, dp);
-    }
-
-    private int func(int idx, int buy, int[] arr, int[][] dp){
-        if(idx==arr.length) return 0;
-
-        if(dp[idx][buy]!=Integer.MIN_VALUE) return dp[idx][buy];
-
-        int op1=0 , op2=0;
+        int[][] dp = new int[n+1][2];
         
-        if(buy==0){
-            op1 = 0 + func(idx+1, 0, arr, dp);
-            op2 = -arr[idx] + func(idx+1, 1, arr, dp);
-        }
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<=1; j++){
+                
+                if(j==0){
+                    dp[i][j] = Math.max(0+dp[i+1][0], -prices[i]+dp[i+1][1]);
+                }else{
+                    dp[i][j] = Math.max(0+dp[i+1][1], prices[i]+dp[i+1][0]);
+                }
 
-        if(buy==1){
-            op1 = 0 + func(idx+1, 1, arr, dp);
-            op2 = arr[idx] + func(idx+1, 0, arr, dp);
+            }
         }
-
-        return dp[idx][buy] = Math.max(op1, op2);
+        return dp[0][0];
     }
 }
