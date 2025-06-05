8fc3de33711c6dd96da159fe00bc5e41819c7337
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-05 15:57:31 +0530

    Time: 1 ms (99.77%) | Memory: 42 MB (79.01%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,27 +1,30 @@
 class Solution {
     public int maxProfit(int K, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n+1][2][K+1];
+        
+        int[][] curr = new int[2][K+1];
+        int[][] after = new int[2][K+1];
 
         for(int i = n-1; i>=0; i--){
             for(int k = 0; k<=K; k++){
                 if(k==0){
-                    dp[i][0][0] = dp[i+1][0][0];
-                    dp[i][1][0] = dp[i+1][1][0];
+                    curr[0][0] = after[0][0];
+                    curr[1][0] = after[1][0];
                 }else{
-                    int buy = -prices[i] + dp[i+1][1][k];
-                    int skip1 = dp[i+1][0][k];
+                    int buy = -prices[i] + after[1][k];
+                    int skip1 = after[0][k];
                     
-                    dp[i][0][k] = Math.max(buy, skip1);
+                    curr[0][k] = Math.max(buy, skip1);
 
-                    int sell = prices[i] + dp[i+1][0][k-1];
-                    int skip2 = dp[i+1][1][k];
+                    int sell = prices[i] + after[0][k-1];
+                    int skip2 = after[1][k];
 
-                    dp[i][1][k] = Math.max(sell, skip2);
+                    curr[1][k] = Math.max(sell, skip2);
                 }
             }
+            after = curr.clone();
         }
-        
-        return dp[0][0][K];
+
+        return after[0][K];
     }
 }
