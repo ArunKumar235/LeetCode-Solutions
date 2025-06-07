55c711e237da187ac3651dfb1a17f279f0958991
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-07 14:32:17 +0530

    Time: 75 ms (23.60%) | Memory: 77.5 MB (15.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,24 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] memo = new int[n][n];
-        for(int[] arr: memo){
-            Arrays.fill(arr, -1);
+        int dp[][]=new int[n+1][n+1];
+
+        for(int ind = n-1; ind>=0; ind--){
+            for(int prev_index = ind-1; prev_index >=-1; prev_index--){
+                
+                int notTake = 0 + dp[ind+1][prev_index +1];
+        
+                int take = 0;
+        
+                if(prev_index == -1 || nums[ind] > nums[prev_index]){                    
+                    take = 1 + dp[ind+1][ind+1];
+                }
+        
+                dp[ind][prev_index+1] = Math.max(notTake,take);
+                
+            }
         }
-        return func(0, -1, nums, memo);
-    }
-
-    private int func(int i, int prev, int[] nums, int[][] memo){
-        if(i==nums.length) return 0;
-        if(memo[i][prev+1] != -1) return memo[i][prev+1];
-
-        int dontTake = func(i+1, prev, nums, memo);
-        int take = 0;
-
-        if(prev == -1 || nums[prev]<nums[i]){
-            take = 1+ func(i+1, i, nums, memo);
-        }
-
-        return memo[i][prev+1] = Math.max(take, dontTake);
+        
+        return dp[0][0];
     }
 }
