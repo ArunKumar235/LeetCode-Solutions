d52e4ad8ac44360236a2e2673aafe6b9619aaa69
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-07 20:20:45 +0530

    Time: 2 ms (96.54%) | Memory: 133.9 MB (52.02%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,36 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length;
-        List<Integer> li = new ArrayList<>();
-        li.add(nums[0]);
-        
-        for(int i = 1; i<n; i++){
-            
-            if(li.get(li.size()-1) < nums[i]){
-                li.add(nums[i]);
-                if(li.size()==3) return true;
-            }else{
-                int idx = binarySearch(li, nums[i]);
-                li.set(idx, nums[i]);
-            }
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-        }
+        for(int num: nums){
+            if(num<=min1){
+                min1 = num;
+            }else if(num<=min2){
+                min2 = num;
+            }else{
+                return true;
+            }
+        }   
         return false;
-    }
-
-    private int binarySearch(List<Integer> li, int val){
-        int start = 0;
-        int end = li.size()-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(li.get(mid)==val){
-                return mid;
-            }else if(li.get(mid)<val){
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-        }
-        return start;
     }
 }
