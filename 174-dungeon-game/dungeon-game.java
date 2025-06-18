7de13e9914fd9a5c0632050397e4568680de5367
Arun Kumar M <marunkumar0203@gmail.com>
2025-06-18 10:59:30 +0530

    Time: 2 ms (70.04%) | Memory: 45.2 MB (62.81%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,20 +1,18 @@
 class Solution {
-    public int calculateMinimumHP(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][] dp = new int[m+1][n+1];
-        for(int[] li: dp) Arrays.fill(li,-1);
-        return func(arr, 0, 0, m, n, dp);
-    }
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m =  dungeon.length;
+        int n = dungeon[0].length;
 
-    private int func(int[][] arr, int i, int j, int m, int n, int[][] dp){
-        if(i == m || j == n) return (int)1e9;
-        if(i == m-1 && j == n-1) return arr[i][j] > 0 ? 1 : 1 - arr[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = func(arr,i+1,j,m,n,dp);
-        int right = func(arr,i,j+1,m,n,dp);
-        int res = Math.min(down,right)-arr[i][j];
-        return dp[i][j] = res > 0 ? res : 1;
+        int dp[][] = new int[m+1][n+1];
+        for (int row[] : dp)  Arrays.fill(row, (int)1e9);
+        dp[m][n - 1] = 1;
+        dp[m - 1][n] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int minHealth = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                dp[i][j] = Math.max(1, minHealth);
+            }
+        }
+        return dp[0][0];
     }
 }
