50dc9d0fb87674bad309069f9725129a9c222a2b
Arun Kumar M <marunkumar0203@gmail.com>
2026-04-27 13:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (32.99%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind1 = -1;
-        int ind2 = -1;
-        
-        for(int i = nums.length-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                ind1 = i;
+        int i1 = -1;
+        int i2 = -1;
+        int n = nums.length;
+        for(int i = n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                i1 = i;
+                break;
+            }
+        }   
+        if(i1==-1){
+            reverse(nums, 0);
+            return;
+        }
+        for(int i = n-1; i>i1; i--){
+            if(nums[i1]<nums[i]){
+                i2 = i;
                 break;
             }
         }
-        if(ind1==-1){
-            reverse(nums,0);
-        }else{
-            for(int i = nums.length-1; i>ind1; i--){
-                if(nums[i]>nums[ind1]){
-                    ind2 = i;
-                    break;
-                }
-            }
-            swap(nums,ind1,ind2);
-            reverse(nums,ind1+1);
-        }
-    }
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        swap(nums, i1, i2);
+        reverse(nums, i1+1);
     }
 
-    public void reverse(int[] nums, int start){
-        int end = nums.length-1;
-        while(start<end){
-            swap(nums, start, end);
-            start++;
-            end--;
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+    private static void reverse(int[] nums, int s){
+        int e = nums.length-1;
+        while(s<e){
+            swap(nums, s++, e--);
         }
     }
 }
