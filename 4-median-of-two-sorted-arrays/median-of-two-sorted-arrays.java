0338551550af1244348551ef5302f17dbe1ba0bb
Arun Kumar M <marunkumar0203@gmail.com>
2026-04-27 20:28:19 +0530

    Time: 1 ms (100.00%) | Memory: 49 MB (33.59%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,28 +1,33 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int arr[] = new int[(nums1.length + nums2.length)/2 + 1];
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int a = arr.length;
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while(i<n1 && j<n2 && k<a){
-            if(nums1[i] < nums2[j]){
-                arr[k++] = nums1[i++];
+    public double findMedianSortedArrays(int[] a, int[] s) {
+        int m = a.length;
+        int n = s.length;
+        // making a the smallest sized array always
+        if(m>n) return findMedianSortedArrays(s, a);
+
+        int left = (m+n+1)/2;
+
+        int l = 0;
+        int h = m;
+        while(l<=h){
+            int m1 = l + (h-l)/2;
+            int m2 = left-m1;
+
+            int l1 = (m1>0) ? a[m1-1] : Integer.MIN_VALUE;
+            int r1 = (m1<m) ? a[m1] : Integer.MAX_VALUE;
+
+            int l2 = (m2>0) ? s[m2-1] : Integer.MIN_VALUE;
+            int r2 = (m2<n) ? s[m2] : Integer.MAX_VALUE;
+
+            if(l1 > r2){
+                h = m1-1;
+            }else if( l2 > r1){
+                l = m1+1;
             }else{
-                arr[k++] = nums2[j++];
+                if((m+n) % 2==1) return Math.max(l1,l2);
+                return (Math.max(l1,l2)+Math.min(r1,r2))/2.0d;
             }
         }
-        while(i<n1 && k<a){
-            arr[k++] = nums1[i++];
-        }
-        while(j<n2 && k<a){
-            arr[k++] = nums2[j++];
-        }
-        if((n1+n2) % 2 == 0){
-            return (arr[a-1]+arr[a-2])/2.0;
-        }
-        return arr[arr.length-1];
+        return -1;
     }
 }
