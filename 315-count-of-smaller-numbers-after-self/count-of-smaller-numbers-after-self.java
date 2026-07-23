24a7856f850c947b84f331174e8a1e4856dd11fb
Arun Kumar M <marunkumar0203@gmail.com>
2026-07-23 11:31:25 +0530

    Time: 73 ms (51.27%) | Memory: 98.5 MB (66.13%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,29 +1,74 @@
 class Solution {
-    public List<Integer> countSmaller(int[] nums) {
-        List<Integer> li = new ArrayList<>();
-        List<Integer> sorted = new ArrayList<>();
-        
-        for(int i = nums.length-1; i>=0; i--){
-            int idx = insert(sorted, nums[i]);
-            li.add(idx);
-            sorted.add(idx, nums[i]);
+
+    class Pair{
+        int val;
+        int idx;
+        Pair(int val, int idx){
+            this.val = val;
+            this.idx = idx;
         }
-        Collections.reverse(li);
-        return li;
     }
 
-    private int insert(List<Integer> arr, int num){
-        int l = 0;
-        int r = arr.size()-1;
+    int[] res;
 
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(arr.get(mid)<num){
-                l = mid+1;
+    public List<Integer> countSmaller(int[] nums) {
+        res = new int[nums.length];
+        
+        Pair[] li = new Pair[nums.length];
+        for(int i = 0; i<nums.length; i++){
+            li[i] = new Pair(nums[i], i);
+        }
+
+        mergeSort(li, 0, nums.length-1);
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i: res){
+            ans.add(i);
+        }
+        return ans;
+    }
+
+    private void mergeSort(Pair[] nums, int l, int r){
+        if(l>=r) return;
+
+        int m = l+(r-l)/2;
+
+        mergeSort(nums, l, m);
+        mergeSort(nums, m+1, r);
+
+        merge(nums, l, m, r);
+    }
+
+    private void merge(Pair[] nums, int l, int m, int r){
+        Pair[] temp = new Pair[r-l+1];
+
+        int i = l;
+        int j = m+1;
+        int k = 0;
+
+        int rightCount = 0;
+
+        while(i<=m && j<=r){
+            if(nums[j].val < nums[i].val){
+                temp[k++] = nums[j++];
+                rightCount++;
             }else{
-                r = mid-1;
+                res[nums[i].idx] += rightCount;
+                temp[k++] = nums[i++];
             }
         }
-        return l;
+
+        while(i<=m){
+            res[nums[i].idx] += rightCount;
+            temp[k++] = nums[i++];
+        }
+
+        while(j<=r){
+            temp[k++] = nums[j++];
+        }
+
+        for(int x = 0; x<temp.length; x++){
+            nums[l+x] = temp[x];
+        }
     }
 }
