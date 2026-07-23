f76520f7aa734d21cf97773fb24df9a64860f1a3
Arun Kumar M <marunkumar0203@gmail.com>
2026-07-23 13:09:43 +0530

    Time: 77 ms (38.64%) | Memory: 79.5 MB (99.79%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,18 +1,63 @@
 class Solution {
+    int lower;
+    int upper;
+    long count = 0;
     public long countFairPairs(int[] nums, int lower, int upper) {
-        Arrays.sort(nums);
-        return getPairsLessThan(nums, upper+1)-getPairsLessThan(nums, lower);    
+        this.lower = lower;
+        this.upper = upper;
+
+        mergeSort(nums, 0, nums.length-1);
+        return this.count;
     }
-    public long getPairsLessThan(int nums[], int val){
-        int left = 0, right = nums.length-1;
-        long res = 0;
-        while(left<=right){
-            if((nums[left]+nums[right])<val){
-                res += right-left;
-                left++;
+
+    private void mergeSort(int[] nums, int l, int r){
+        if(l>=r){
+            return;
+        }
+        int m = l+(r-l)/2;
+        mergeSort(nums, l, m);
+        mergeSort(nums, m+1, r);
+        countPairs(nums, l, m, r);
+        merge(nums, l, m, r);
+    }
+
+    private void countPairs(int[] nums, int l, int m, int r){
+        int low = m+1;
+        int high = m+1;
+
+        for(int i = m; i>=l; i--){
+            while(low<=r && (long)nums[i]+nums[low] < this.lower){
+                low++;
             }
-            else right--;
-        }  
-        return res;
+            while(high<=r && (long)nums[i]+nums[high] <= this.upper){
+                high++;
+            }
+            this.count += high-low;
+        }
+    }
+
+    private void merge(int[] nums, int l, int m, int r){
+        int[] temp = new int[r-l+1];
+        int i = l;
+        int j = m+1;
+        int k = 0;
+
+        while(i<=m && j<=r){
+            if(nums[i]<=nums[j]){
+                temp[k++] = nums[i++];
+            }else{
+                temp[k++] = nums[j++];
+            }
+        }
+        while(i<=m){
+            temp[k++] = nums[i++];
+        }
+        while(j<=r){
+            temp[k++] = nums[j++];
+        }
+        
+        for(int x = 0; x<temp.length; x++){
+            nums[l+x] = temp[x];
+        }
     }
 }
