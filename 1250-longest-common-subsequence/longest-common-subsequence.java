8b825180e462e22eb103e6f869d7e12ee8031ad0
Arun Kumar M <marunkumar0203@gmail.com>
2026-07-27 00:17:48 +0530

    Time: 20 ms (78.45%) | Memory: 53.8 MB (69.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,19 +2,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int l1 = text1.length();
         int l2 = text2.length();
-        int[] prev = new int[l2+1];
-        int[] curr = new int[l2+1];
+        int[][] dp = new int[l1+1][l2+1];
+
         for(int i = 1; i<=l1; i++){
             for(int j = 1; j<=l2; j++){
                 if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    curr[j] = 1+prev[j-1];
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }else{
-                    curr[j] = Math.max(prev[j], curr[j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            prev = curr;
-            curr = new int[l2+1];
         }
-        return prev[l2];
+        return dp[l1][l2];
     }
 }
