e621ab75a60f911da21707f2dba6c8851c43ac15
Arun Kumar M <marunkumar0203@gmail.com>
2026-07-26 12:26:39 +0530

    Time: 168 ms (22.34%) | Memory: 115 MB (11.01%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,18 +1,22 @@
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int num: nums){
-            min = Math.min(min, num);
-            max = Math.max(max, num);
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
+        for(int num: sorted){
+            if(!map.containsKey(num)){
+                map.put(num, idx++);
+            }
         }
+        SegmentTree st = new SegmentTree(0, idx-1);
         List<Integer> res = new ArrayList<>();
 
-        SegmentTree st = new SegmentTree(min, max);
-        
         for(int i = nums.length-1; i>=0; i--){
-            st.add(nums[i]);
-            int cnt = st.querySum(min, nums[i]-1);
+            int rank = map.get(nums[i]);
+            st.add(rank);
+            int cnt = st.query(0, rank-1);
             res.add(cnt);
         }
         return res.reversed();
@@ -21,64 +25,57 @@ class Solution {
 
 class SegmentTree{
     static class Node{
-        public int start;
-        public int end;
+        int start;
+        int end;
 
-        public int freqSum;
+        int freqSum;
+        
         Node left;
-        Node right;
+        Node right;;
 
-        Node(int start, int end){
-            this.start = start;
-            this.end = end;
+        public Node(int s, int e){
+            start = s;
+            end = e;
             freqSum = 0;
         }
     }
-
-    private final Node root;
-
+    Node root;
+    
     public SegmentTree(int s, int e){
         root = constructTree(s, e);
     }
 
     private Node constructTree(int s, int e){
-        if(s>e) return null;
-        if(s==e){
-            return new Node(s, e);
-        }
-        Node curr = new Node(s, e);
-        int mid = s+ (e-s)/2;
-        curr.left = constructTree(s, mid);
-        curr.right = constructTree(mid+1, e);
-        // curr.freqSum = curr.left.freqSum + curr.right.freqSum;
-        return curr;
+        if(s==e) return new Node(s, e);
+        Node node = new Node(s, e);
+        int mid = s + (e-s)/2;
+        node.left = constructTree(s, mid);
+        node.right = constructTree(mid+1, e);
+        return node;
     }
 
-    public int add(int val){
-        return update(root, val);
+    public void add(int val){
+        update(root, val);
     }
 
     private int update(Node node, int val){
         if(node==null) return 0;
-        if(val < node.start || val > node.end){
-            return node.freqSum;
-        }
-        if(node.start == node.end){
+        if(val < node.start || node.end < val) return node.freqSum;
+
+        if(node.start == val && node.end == val){
             return ++node.freqSum;
         }
         node.freqSum = update(node.left, val) + update(node.right, val);
         return node.freqSum;
     }
 
-    public int querySum(int s, int e){
-        if(s > e){
-            return 0;
-        }
+    public int query(int s, int e){
+        if(s > e) return 0;
         return query(root, s, e);
     }
 
     private int query(Node node, int s, int e){
-        if(node == null || node.start > e || node.end < s){
+        if(node == null || e < node.start || node.end < s){
             return 0;
         }
         if(s <= node.start && node.end <= e){
