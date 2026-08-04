2a06c374a470afa0c908df4a8b76eeb7a8d76399
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-04 16:14:48 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (99.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,55 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
+        Queue<int[]> q = new ArrayDeque<>();
         int fresh = 0;
-        Queue<orange> queue = new ArrayDeque<>();
-        for(int i= 0;i <r;i++){
-            for(int j=0;j<c;j++){
-                if(grid[i][j] == 1){
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j, 0});
+                }else if(grid[i][j] == 1){
                     fresh++;
-
-                }
-                else if(grid[i][j] == 2){
-                    queue.add(new orange(i,j,0));
-                    grid[i][j] = 0;
                 }
             }
         }
-        if(queue.isEmpty() && fresh == 0){
-            return 0;
-        }
-        int maxMins = 0;
-        int rowdiff[] = {-1,0,0,1};
-        int coldiff[] = {0,-1,1,0};
-
-        while(!queue.isEmpty()){
-            orange curr = queue.poll();
-            maxMins = Math.max(maxMins,curr.mins);
-            for(int i=0;i<4;i++){
-                int adjr = curr.row+rowdiff[i];
-                int adjc = curr.col+coldiff[i];
-                if(adjr >= 0 && adjr<r && adjc>=0 && adjc<c && grid[adjr][adjc] == 1){
-                    queue.add(new orange(adjr,adjc,curr.mins+1));
-
+        int mins = 0;
+        int[] rowDiff = new int[]{-1, 0, 1, 0};
+        int[] colDiff = new int[]{0, 1, 0, -1};
+        
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            int min = curr[2];
+            mins = Math.max(mins, min);
+            for(int i = 0; i<4; i++){
+                int newR = r + rowDiff[i];
+                int newC = c + colDiff[i];
+                if( 0 <= newR && newR < grid.length &&
+                    0 <= newC && newC < grid[0].length &&
+                    grid[newR][newC] == 1
+                ){
+                    q.offer(new int[]{newR, newC, min+1});
                     fresh--;
-                    grid[adjr][adjc] = 0;
+                    grid[newR][newC] = 2;
                 }
             }
         }
-        if(fresh>0){
-            return -1;
-        }
-        return maxMins;
-    }
-}
-
-
-class orange{
-    int row,col,mins;
-    public orange(int r,int c,int m){
-        this.row = r;
-        this.col = c;
-        this.mins= m;
+        return fresh !=0 ? -1 : mins;
     }
 }
