ed874997f920e21c33057895e407fdaedc20cd08
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-05 23:19:21 +0530

    Time: 1 ms (99.82%) | Memory: 47.9 MB (22.99%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,34 +1,42 @@
 class Solution {
+    int[] rowDiff = new int[]{-1, 0, 1, 0};
+    int[] colDiff = new int[]{0, 1, 0, -1};
+
     public void solve(char[][] board) {
-        int r = board.length;
-        int c = board[0].length;
-        for(int i = 0; i<r; i++){
-            dfs(board,i,0);
+        int R = board.length;
+        int C = board[0].length;
+
+        for(int i = 0; i<R; i++){
+            if(board[i][0] == 'O') dfs(i, 0, board);
+            if(board[i][C-1] == 'O') dfs(i, C-1, board);
         }
-        for(int i = 0; i<r; i++){
-            dfs(board,i,c-1);
+        for(int j = 0; j<C; j++){
+            if(board[0][j] == 'O') dfs(0, j, board);
+            if(board[R-1][j] == 'O') dfs(R-1, j, board);
         }
-        for(int i = 0; i<c; i++){
-            dfs(board,0,i);
-        }
-        for(int i = 0; i<c; i++){
-            dfs(board,r-1,i);
-        }
-        for(int i = 0; i<r; i++){
-            for(int j = 0; j<c; j++){
-                if(board[i][j]=='I') board[i][j] = 'O';
-                else board[i][j] = 'X';
+
+        for(int i = 0; i<R; i++){
+            for(int j = 0; j<C; j++){
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == 'A') board[i][j] = 'O';
             }
         }
+        return;
     }
 
-    public void dfs(char[][] board, int row, int col){
-        if(row==board.length || col==board[0].length || row==-1 || col==-1) return;
-        if(board[row][col]=='X' || board[row][col]=='I') return;
-        board[row][col] = 'I';
-        dfs(board, row, col-1);
-        dfs(board, row, col+1);
-        dfs(board, row-1, col);
-        dfs(board, row+1, col);
+    private void dfs(int r, int c, char[][] board){
+        board[r][c] = 'A';
+
+        for(int i = 0; i<4; i++){
+            int newR = r + rowDiff[i];
+            int newC = c + colDiff[i];
+
+            if( 0 <= newR && newR < board.length &&
+                0 <= newC && newC < board[0].length &&
+                board[newR][newC] == 'O'
+            ){
+                dfs(newR, newC, board);
+            }
+        }
     }
 }
