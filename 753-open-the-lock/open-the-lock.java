c9ca66a65e99edf1fa34fcd930c9571be3088031
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-06 23:10:27 +0530

    Time: 84 ms (76.08%) | Memory: 48.1 MB (88.20%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        if(target.equals("0000")) return 0;
+
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        
+        if(visited.contains("0000")) return -1;
+
+        Queue<String> q = new ArrayDeque<>();
+        q.offer("0000");
+
+        int steps = 0;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            steps++;
+            
+            for(int s = 0; s<size; s++){
+                String lock = q.poll();
+                
+                for(int c = 0; c<4; c++){
+                    
+                    for(int i = -1; i <= 1; i+=2){
+                        char[] arr = lock.toCharArray();
+                        arr[c] = (char) (((arr[c] - '0' + i + 10) % 10) + '0');
+                        
+                        String nextLock = new String(arr);
+                        
+                        if(nextLock.equals(target)) return steps;
+                        
+                        if(!visited.contains(nextLock)){
+                            q.offer(nextLock);
+                            visited.add(nextLock);
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
