f68a751df9deb5d1d4acbbc31c60ac6760f625bd
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-07 14:35:12 +0530

    Time: 5 ms (82.10%) | Memory: 46.8 MB (52.36%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i<numCourses; i++) adj.add(new ArrayList<>());
+
+        for(int[] pre: prerequisites) adj.get(pre[0]).add(pre[1]);
+
+        int[] state = new int[numCourses];
+        // 0 unvisited
+        // 1 visiting
+        // 2 visited
+
+        Set<Integer> order = new LinkedHashSet<>();
+        
+        for(int i = 0; i<numCourses; i++){
+            if(!dfs(i, adj, state, order)) break;
+        }
+
+
+       if(order.size()!=numCourses) return new int[]{};
+
+       int i = 0;
+       for(int course: order) state[i++] = course;
+       return state;
+    }
+
+    private boolean dfs(int course, List<List<Integer>> adj, int[] state, Set<Integer> order){
+        if(state[course] == 1) return false;
+
+        if(state[course] == 2) return true;
+
+        state[course] = 1;
+
+        for(int next: adj.get(course)){
+            if(!dfs(next, adj, state, order)) return false;
+        }
+        
+        order.add(course);
+        state[course] = 2;
+
+        return true;
+    }
+}
