c0d951649f0e31a81f8c01f6e7acf00545bb732b
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-13 21:28:41 +0530

    Time: 15 ms (90.42%) | Memory: 46.7 MB (20.84%) - LeetSync

---
+++ 4310-count-good-integers-in-a-range/count-good-integers-in-a-range.java
@@ -0,0 +1,49 @@
+class Solution {
+    public long goodIntegers(long l, long r, int k) {
+        return count(r, k) - count(l-1, k);
+    }
+
+    private long count(long num, long k){
+        String str = String.valueOf(num);
+        int len = str.length();
+        long[][][][] dp = new long[len][2][2][11];
+        for(int i = 0; i<len; i++){
+            for(int j = 0; j<2; j++){
+                for(int r = 0; r<2; r++) Arrays.fill(dp[i][j][r], -1);
+            }
+        }
+        return func(str, 0, 1, 1, 10, k, dp);
+    }
+    // tight - 0 - prev values is changed
+    // tight - 1 - prev values not changed, so limit to be followed
+
+    // begin - 1 - leading digits only 0's 
+    // begin - 0 - number formation started
+    private long func(String str, int idx, int tight, int begin, int prev, long k, long[][][][] dp){
+        if(idx == str.length()){
+            return begin == 1 ? 0 : 1;
+        }
+
+        if(tight == 0 && dp[idx][tight][begin][prev] != -1) return dp[idx][tight][begin][prev];
+
+        long ans = 0;
+
+        int limit = tight == 0 ? 9 : str.charAt(idx) - '0';
+
+        for(int d = 0; d<=limit; d++){
+            int newTight = (tight == 1 && d == limit) ? 1 : 0;
+
+            if(begin == 1 && d == 0){
+                ans += func(str, idx+1, newTight, 1, 10, k, dp);
+            }else{
+                if(prev == 10 || Math.abs(prev-d) <= k){
+                    ans += func(str, idx+1, newTight, 0, d, k, dp);
+                }
+            }
+        } 
+        if(tight == 0){
+            dp[idx][tight][begin][prev] = ans;
+        }
+        return ans;
+    }
+}
