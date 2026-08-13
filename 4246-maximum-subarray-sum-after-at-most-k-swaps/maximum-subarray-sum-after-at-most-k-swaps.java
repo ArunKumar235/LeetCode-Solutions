5e09906073bd5f6c03100dee218953a6a4d01f44
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-13 20:03:11 +0530

    Time: 72 ms (81.99%) | Memory: 47.3 MB (64.80%) - LeetSync

--- 4246-maximum-subarray-sum-after-at-most-k-swaps/maximum-subarray-sum-after-at-most-k-swaps.java
+++ 4246-maximum-subarray-sum-after-at-most-k-swaps/maximum-subarray-sum-after-at-most-k-swaps.java
@@ -1,88 +1,74 @@
 class Solution {
     public long maxSum(int[] nums, int k) {
         int n = nums.length;
-
+        
         int negatives = 0;
-        for(int num : nums){
-            if(num < 0) negatives++;
-        }
-
-        if(negatives == n){
+        for(int num: nums) if(num<0) negatives++;
+        
+        if(negatives==n){
             int max = nums[0];
-            for(int num : nums){
-                max = Math.max(max, num);
-            }
+            for(int num: nums) max = Math.max(max, num);
             return max;
         }
 
-        if(negatives <= k){
-            long sum = 0;
-            for(int num : nums){
-                if(num >= 0) sum += num;
-            }
+        if(negatives<=k){
+            int sum = 0;
+            for(int num: nums) if(0<num) sum += num;
             return sum;
         }
-
-        long[] prefix = new long[n + 1];
-        for(int i = 0; i < n; i++){
-            prefix[i + 1] = prefix[i] + nums[i];
+        
+        long[] prefix = new long[n];
+        for(int i = 0; i<n; i++){
+            if(i==0) prefix[0] = nums[0];
+            else prefix[i] = prefix[i-1] + nums[i];
         }
 
         long res = Long.MIN_VALUE;
 
-        long[] remove = new long[n];
-
-        for(int l = 0; l < n; l++){
-
+        for(int l = 0; l<n; l++){
+            
             PriorityQueue<Integer> inside = new PriorityQueue<>();
             long insideSum = 0;
-
-            for(int r = l; r < n; r++){
+            
+            long[] remove = new long[n];
+            
+            for(int r = l; r<n; r++){
                 if(nums[r] < 0){
-                    inside.offer(-nums[r]);
                     insideSum += -nums[r];
-
-                    if(inside.size() > k){
-                        insideSum -= inside.poll();
-                    }
+                    inside.offer(-nums[r]);
                 }
+                if(inside.size() > k) insideSum -= inside.poll();
+
                 remove[r] = insideSum;
             }
 
             PriorityQueue<Integer> outside = new PriorityQueue<>();
             long outsideSum = 0;
 
-            for(int i = 0; i < l; i++){
-                if(nums[i] >= 0){
-                    outside.offer(nums[i]);
+            for(int i = 0; i<l; i++){
+                if(0 <= nums[i]){
                     outsideSum += nums[i];
-
-                    if(outside.size() > k){
-                        outsideSum -= outside.poll();
-                    }
+                    outside.offer(nums[i]);
                 }
+                if(outside.size() > k) outsideSum -= outside.poll();
             }
 
-            for(int r = n - 1; r >= l; r--){
-
-                long subSum = prefix[r+1] - prefix[l];
+            for(int r = n-1; r>=l; r--){
+                long subSum = prefix[r];
+                if(l > 0) subSum = prefix[r] - prefix[l-1];
 
                 res = Math.max(
                     res,
-                    subSum + remove[r] + outsideSum
+                    subSum + remove[r] + outsideSum 
                 );
 
                 if(nums[r] >= 0){
-                    outside.offer(nums[r]);
                     outsideSum += nums[r];
-
-                    if(outside.size() > k){
-                        outsideSum -= outside.poll();
-                    }
+                    outside.offer(nums[r]);
                 }
+                if(outside.size() > k) outsideSum -= outside.poll();
             }
         }
-
         return res;
     }
 }
