3675a6994047abdfda6ecdb0be79b9f2eb1c73cd
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-14 17:37:38 +0530

    Time: 11 ms (27.96%) | Memory: 45.8 MB (27.31%) - LeetSync

--- 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.java
@@ -3,61 +3,56 @@ class Solution {
         return n - countUnique(n);
     }
 
-    private int countUnique(int num){
-        String str = String.valueOf(num);
+    private int countUnique(int n){
+        String str = String.valueOf(n);
         int len = str.length();
-        
-        // idx - tight - begin - bitMask (1024 - 10000000000)
-        int[][][][] dp = new int[len][2][2][1 << 10];
+
+        // idx - tight - bitMask (1024 - 10000000000)
+        int[][][] dp = new int[len][2][1 << 10];
 
         for(int i = 0; i < len; i++){
             for(int j = 0; j < 2; j++){
-                for(int k = 0; k < 2; k++){
-                    Arrays.fill(dp[i][j][k], -1);
-                }
+                Arrays.fill(dp[i][j], -1);
             }
         }
-        return func(str, 0, 0, 0, 0, dp);
+        return func(str, 0, 0, 0, dp);
     }
 
     // tight - 0 - previous digits equal to n
     // tight - 1 - already smaller than n
 
-    // begin - 0 - number has not started
-    // begin - 1 - number has started
+    // mask - 0  - begin - 0 - number has not started
+    // mask - !0 - begin - 1 - number has started
 
     // mask:
     // bit - 0 - digit unused
     // bit - 1 - digit already used
 
-    private int func(String str, int idx, int tight, int begin, int mask, int[][][][] dp){
-
+    private int func(String str, int idx, int tight, int mask, int[][][] dp){
         if(idx == str.length()){
-            return begin == 1 ? 1 : 0;
+            return mask == 0 ? 0 : 1;
         }
 
-        if(dp[idx][tight][begin][mask] != -1) return dp[idx][tight][begin][mask];
+        if(dp[idx][tight][mask] != -1) return dp[idx][tight][mask];
 
-        int limit = (tight == 1) ? 9 : str.charAt(idx) - '0';
+        int limit = tight == 1 ? 9 : str.charAt(idx) - '0';
 
         int ans = 0;
 
         for(int d = 0; d <= limit; d++){
-            int newTight = (tight == 0 && d == limit) ? 0 : 1;
+            int newTight = (tight == 0 && d < limit) ? 1 : tight;
 
-            if(begin == 0 && d == 0){
-                ans += func(str, idx+1, newTight, 0, mask, dp);
+            if(mask == 0 && d == 0){
+                ans += func(str, idx + 1, newTight, 0, dp);
                 continue;
             }
-
-            if((mask & (1 << d)) != 0){ // digit occured already, so not unique
+            if((mask & (1 << d)) != 0){ // digit already occured, so not unique
                 continue;
             }
-
             int newMask = mask | (1 << d);
 
-            ans += func(str, idx+1, newTight, 1, newMask, dp);
+            ans += func(str, idx + 1, newTight, newMask, dp);
         }
-        return dp[idx][tight][begin][mask] = ans;
+        return dp[idx][tight][mask] = ans;
     }
 }
