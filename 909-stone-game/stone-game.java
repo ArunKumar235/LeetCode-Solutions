f37df2d062a8deed57a4bf865985f2c8f7346d3f
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-16 21:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.59%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -3,26 +3,14 @@ class Solution {
         int n = piles.length;
         int total = 0;
         for(int pile: piles) total += pile;
-
-        int[][] dp = new int[n][n];
-        for(int i = 0; i<n; i++){
-            Arrays.fill(dp[i], -1);
+        
+        int aliceChoice1 = 0;
+        int aliceChoice2 = 0;
+        for(int i = 0; i<n; i+=2){
+            aliceChoice1 += piles[i];
+            aliceChoice2 += piles[i+1];
         }
 
-        int alice = func(0, n-1, piles, dp);
-        return alice > total - alice;
-    }
-
-    private int func(int l, int r, int[] piles, int[][] dp){
-        if(l > r) return 0;
-
-        if(dp[l][r] != -1) return dp[l][r];
-
-        boolean evenLen = (piles.length % 2) == 0;
-        int first = evenLen ? piles[l] : 0; 
-        int last = evenLen ? piles[r] : 0;
-
-        dp[l][r] = Math.max(first + func(l+1, r, piles, dp), last + func(l, r-1, piles, dp));
-        return dp[l][r];
+        return aliceChoice1 > total - aliceChoice1 || aliceChoice2 > total - aliceChoice2;
     }
 }
