804a187ba855ed5bc768f28acdb191844260ed6a
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-19 21:45:40 +0530

    Time: 31 ms (54.49%) | Memory: 49.4 MB (76.47%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,48 +1,30 @@
 class Solution {
-    Map<String, Integer> emailIdx = new HashMap<>(); // email - idx
-    List<String> emails = new ArrayList<>();
-    Map<Integer, Integer> emailToGrp = new HashMap<>(); // emailIdx - groupIdx
-
-    List<List<Integer>> adj = new ArrayList<>(); // email as nodes
-
-    boolean[] visited;
-
-    Map<Integer, List<String>> emailGroup = new HashMap<>(); // groupIdx - list of emails
-
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int n = 0;
+        Map<String, Integer> emailToGroup = new HashMap<>(); // email - groupIdx
+
+        // groupIdx as nodes for union
+        UnionFind uf = new UnionFind(accounts.size());
+
         for(int groupIdx = 0; groupIdx < accounts.size(); groupIdx++){
             for(int i = 1; i<accounts.get(groupIdx).size(); i++){
                 String email = accounts.get(groupIdx).get(i);
-                if(!emailIdx.containsKey(email)){
-                    emailIdx.put(email, n);
-                    emails.add(email);
-                    emailToGrp.put(n, groupIdx);
-                    n++;
+                
+                if(emailToGroup.containsKey(email)){
+                    uf.union(emailToGroup.get(email), groupIdx); // union (existing group idx - new group idx)
+                }else{
+                    emailToGroup.put(email, groupIdx);
                 }
             }
         }
+        Map<Integer, List<String>> emailGroup = new HashMap<>(); // groupIdx - list of emails
 
-        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
-        visited = new boolean[n];
+        for(Map.Entry<String, Integer> entry: emailToGroup.entrySet()){
+            String email = entry.getKey();
+            int groupIdx = entry.getValue();
+            int orginalIdx = uf.find(groupIdx);
 
-        // emails within same group are connected
-        for(int groupIdx = 0; groupIdx < accounts.size(); groupIdx++){
-            for(int i = 2; i<accounts.get(groupIdx).size(); i++){
-                int u = emailIdx.get(accounts.get(groupIdx).get(i-1));
-                int v = emailIdx.get(accounts.get(groupIdx).get(i));
-
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-        }
-
-        for(int i = 0; i<n; i++){
-            if(!visited[i]){
-                int groupIdx = emailToGrp.get(i);
-                emailGroup.putIfAbsent(groupIdx, new ArrayList<>());
-                dfs(i, groupIdx);
-            }
+            emailGroup.putIfAbsent(orginalIdx, new ArrayList<>());
+            emailGroup.get(orginalIdx).add(email);
         }
 
         List<List<String>> res = new ArrayList<>();
@@ -56,14 +38,37 @@ class Solution {
         }
         return res;
     }
+}
 
-    private void dfs(int email, int groupIdx){
-        visited[email] = true;
-        emailGroup.get(groupIdx).add(emails.get(email));
-        for(int nei: adj.get(email)){
-            if(!visited[nei]){
-                dfs(nei, groupIdx);
-            }
+class UnionFind{
+    int n;
+    int[] parent;
+
+    UnionFind(int n){
+        this.n = n;
+        parent = new int[n];
+        Arrays.fill(parent, -1);
+    }
+
+    public void union(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu == pv) return;
+
+        if(parent[pu] <= parent[pv]){
+            parent[pu] += parent[pv];
+            parent[pv] = pu;
+            parent[v] = pu;
+        }else{
+            parent[pv] += parent[pu];
+            parent[pu] = pv;
+            parent[v] = pv;
         }
+    }
+
+    public int find(int u){
+        if(parent[u] < 0) return u;
+        return parent[u] = find(parent[u]);
     }
 }
