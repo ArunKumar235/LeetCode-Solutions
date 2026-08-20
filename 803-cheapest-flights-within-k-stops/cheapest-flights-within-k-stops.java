be24a6175fb89214ab3d2b3a07d7659e70001267
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-21 00:49:47 +0530

    Time: 13 ms (11.98%) | Memory: 46.2 MB (92.55%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for(int[] flight: flights){
+            int u = flight[0];
+            int v = flight[1];
+            int price = flight[2];
+
+            adj.putIfAbsent(u, new ArrayList<>());
+            adj.get(u).add(new int[]{v, price});
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        // node - stops
+        int[][] dp = new int[n][k+2];
+        for(int[] row: dp) Arrays.fill(row, Integer.MAX_VALUE);
+
+        // node - price - stops
+        pq.offer(new int[]{src, 0, 0});
+        dp[src][0] = 0;
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int u = curr[0];
+            int price = curr[1];
+            int stop = curr[2];
+
+            if(u == dst) return price;
+
+            if(stop == k+1) continue;
+
+            if(price > dp[u][stop]) continue;
+
+            if(!adj.containsKey(u)) continue;
+            for(int[] nei: adj.get(u)){
+                int v = nei[0];
+                int newPrice = price + nei[1];
+                int newStop = stop + 1;
+                
+                if(newPrice < dp[v][newStop]){
+                    dp[v][newStop] = newPrice;
+                    pq.offer(new int[]{v, newPrice, newStop});
+                }
+            }
+        }
+        return -1;
+    }
+}
