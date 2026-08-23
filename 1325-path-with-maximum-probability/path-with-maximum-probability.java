d3f7047d5b7547541869275e6df7248935d8736d
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-23 18:15:11 +0530

    Time: 27 ms (84.38%) | Memory: 72.5 MB (24.78%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,34 +1,44 @@
-// bellman-ford
+// dijkistra
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double[] dp = new double[n];
-        Arrays.fill(dp, 0.0);
-        dp[start_node] = 1.0;
+        List<List<double[]>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++) adj.add(new ArrayList<>());
 
-        for(int i = 0; i < n-1; i++){
-            boolean updated = false;
+        for(int i = 0; i<edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
 
-            for(int j = 0; j < edges.length; j++){
-                int u = edges[j][0];
-                int v = edges[j][1];
-                double prob = succProb[j];
+            adj.get(u).add(new double[]{v, prob});
+            adj.get(v).add(new double[]{u, prob});
+        }
 
-                // u -> v
-                if(dp[u] * prob > dp[v]){
-                    dp[v] = dp[u] * prob;
-                    updated = true;
+        double[] maxProb = new double[n];
+        maxProb[start_node] = 1.0;
+
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[1], a[1]));
+
+        pq.offer(new double[]{start_node, 1.0});
+
+        while(!pq.isEmpty()){
+            double[] curr = pq.poll();
+            int node = (int) curr[0];
+            double prob = curr[1];
+
+            if(prob < maxProb[node]) continue;
+
+            if(node == end_node) return prob;
+
+            for(double[] edge : adj.get(node)){
+                int next = (int) edge[0];
+                double newProb = prob * edge[1];
+
+                if (newProb > maxProb[next]) {
+                    maxProb[next] = newProb;
+                    pq.offer(new double[]{next, newProb});
                 }
-
-                // v -> u
-                if(dp[v] * prob > dp[u]){
-                    dp[u] = dp[v] * prob;
-                    updated = true;
-                }
-            }
-            if(!updated){
-                break;
             }
         }
-        return dp[end_node];
+        return 0.0;
     }
 }
