e9e572df0f5dcb22f7a2a58d0e46e9e49b0372d2
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-23 16:23:58 +0530

    Time: 88 ms (99.25%) | Memory: 146.7 MB (12.67%) - LeetSync

---
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        int[] Q = queries.clone();
+        Arrays.sort(Q);
+
+        // interval size - right interval
+        PriorityQueue<int[]> eligible = new PriorityQueue<>((a, b) ->{
+            return a[0] - b[0];
+        });
+
+        int i = 0;
+        Map<Integer, Integer> res = new HashMap<>();
+
+        for(int q: Q){
+            while(i < intervals.length && intervals[i][0] <= q){
+                int l = intervals[i][0];
+                int r = intervals[i++][1];
+
+                eligible.offer(new int[]{r-l +1, r});
+            }
+
+            while(!eligible.isEmpty() && eligible.peek()[1] < q){
+                eligible.poll();
+            }
+            res.put(q, eligible.isEmpty() ? -1 : eligible.peek()[0]);
+        }
+
+        i = 0;
+        int[] answer = new int[queries.length];
+        for(int q: queries){
+            answer[i++] = res.get(q);
+        }
+        return answer;
+    }
+}
