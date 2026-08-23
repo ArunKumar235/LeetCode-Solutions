1bd1383e9dd54850343e33cec15142f722cde22f
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-23 16:25:46 +0530

    Time: 95 ms (94.19%) | Memory: 146.2 MB (17.73%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
@@ -7,6 +7,7 @@ class Solution {
 
         // interval size - right interval
         PriorityQueue<int[]> eligible = new PriorityQueue<>((a, b) ->{
+            if(a[0] == b[0]) return a[1] - b[1];
             return a[0] - b[0];
         });
 
