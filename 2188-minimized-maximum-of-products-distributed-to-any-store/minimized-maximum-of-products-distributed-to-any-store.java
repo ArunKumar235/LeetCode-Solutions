8267f2b1cff2e06636b034f6a4474b80936dff06
Arun Kumar M <marunkumar0203@gmail.com>
2026-08-28 00:39:51 +0530

    Time: 23 ms (87.85%) | Memory: 80.8 MB (93.74%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int max = 0;
+        for(int q : quantities) max = Math.max(max, q);
+
+        int l = 1;
+        int r = max;
+        
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(check(quantities, n, mid)){
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    private boolean check(int[] quants, int n, int maxQuant){
+        int stores = 0;
+
+        for (int q : quants){
+            stores += (q + maxQuant - 1) / maxQuant;
+
+            if(stores > n){
+                return false;
+            }
+        }
+        return true;
+    }
+}
