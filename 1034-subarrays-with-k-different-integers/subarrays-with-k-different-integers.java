18a742b93dd1827a459a906e76a1c071e6420229
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-03 00:18:15 +0530

    Time: 57 ms (5.08%) | Memory: 48.8 MB (94.32%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,23 +1,25 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1); // {<=K} - {<=(K-1)} -> {==K}
+        return atMost(nums, k) - atMost(nums, k-1);
     }
 
-    // Returns the count of all subarrays with <=K unique elements
-    private int helper(int[] nums, int k) { 
-        Map<Integer, Integer> map = new HashMap<>();
-        int res = 0, l = 0;
+    // subarry count with atmost k different integers
+    private int atMost(int[] nums, int k){
+        Map<Integer, Integer> freq = new HashMap<>();
 
-        for (int r = 0; r < nums.length; r++) {
-            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
-            while (map.size() > k) {
-                map.put(nums[l], map.get(nums[l]) - 1);
-                if (map.get(nums[l]) == 0) {
-                    map.remove(nums[l]);
-                }
+        int l = 0;
+        int count = 0;
+        int res = 0;
+        for(int r = 0; r < nums.length; r++){
+            freq.put(nums[r], freq.getOrDefault(nums[r], 0) + 1);
+            if(freq.get(nums[r]) == 1) count++;
+
+            while(count > k){
+                freq.put(nums[l], freq.get(nums[l]) - 1);
+                if(freq.get(nums[l]) == 0) count--;
                 l++;
             }
-            res += (r - l + 1); // Count of subarrays ending at r, and starting anywhere from l to r
+            res += r - l + 1;
         }
         return res;
     }
