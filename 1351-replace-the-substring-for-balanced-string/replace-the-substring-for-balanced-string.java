43ba5a2e677a1e47a377ba3e896cdcfcfd5ebcb1
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-02 23:38:13 +0530

    Time: 10 ms (54.04%) | Memory: 44.6 MB (87.02%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.java
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.java
@@ -1,3 +1,8 @@
+// Choose smallest window
+//         ↓
+// remove its counts from total
+//         ↓
+// everything remaining must have count <= n/4
 class Solution {
     public int balancedString(String s) {
         int n = s.length();
@@ -13,9 +18,14 @@ class Solution {
         for(int r = 0; r < n; r++){
             // r goes inside the window, so count is reduced
             count[s.charAt(r)]--;
+            
             // shrink window when outside is valid
+            // when outside freq is <= k, we could modify inside to any characters of our wish
+            
+            // https://leetcode.com/problems/replace-the-substring-for-balanced-string/solutions/408978/javacpython-sliding-window/comments/1101250/
             while(l < n && count['Q'] <= k && count['W'] <= k && count['E'] <= k && count['R'] <= k){
                 res = Math.min(res, r - l + 1);
+                
                 // l goes outside the window, so count is increased
                 count[s.charAt(l++)]++;
             }
