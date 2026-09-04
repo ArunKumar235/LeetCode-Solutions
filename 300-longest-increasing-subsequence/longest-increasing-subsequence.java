85de564f5b050d4db0b6b6218c4c4747c09a8254
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-04 11:19:27 +0530

    Time: 41 ms (50.96%) | Memory: 46.3 MB (43.69%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,34 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> li = new ArrayList<>();
-        li.add(nums[0]);
+        int n = nums.length;
 
-        for(int i = 1; i<nums.length; i++){
-            if(li.get(li.size()-1) < nums[i]){
-                li.add(nums[i]);
-            }else{
-                int idx = binarySearch(li, nums[i]);
-                li.set(idx, nums[i]);
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[i] < nums[j]) dp[j] = Math.max(dp[j], dp[i]+1);
             }
         }
-        return li.size();
         
-    }
-
-    private int binarySearch(List<Integer> li, int val){
-        int start = 0;
-        int end = li.size()-1;
-        
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(li.get(mid)==val){
-                return mid;
-            }else if(li.get(mid)<val){
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-        }
-        return start;
+        int max = 1;
+        for(int num: dp) max = Math.max(max, num);
+        return max;
     }
 }
