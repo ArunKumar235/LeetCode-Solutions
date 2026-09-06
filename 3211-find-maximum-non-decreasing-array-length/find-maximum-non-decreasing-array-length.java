476cc59b64ac76c9018d39af8bd83bb1ba3b22d4
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-07 01:02:43 +0530

    Time: 1822 ms (5.26%) | Memory: 141.5 MB (7.89%) - LeetSync

--- 3211-find-maximum-non-decreasing-array-length/find-maximum-non-decreasing-array-length.java
+++ 3211-find-maximum-non-decreasing-array-length/find-maximum-non-decreasing-array-length.java
@@ -10,30 +10,31 @@ class Solution {
         int[] dp = new int[n+1];
         
         // threshold, idx
-        // threshold tells us the minimum future prefix sum required before this index can be used as prev
+        // threshold tells us the minimum future prefix sum required before this index can be used as l
         // maintain thresholds in increasing order
         Deque<long[]> dq = new ArrayDeque<>();
 
-        int prev = 0;
+        int l = 0;
 
         for(int r = 1; r <= n; r++){
             // if prefix[r] >= threshold then that candidate has become valid
             // because thresholds are increasing, all candidates that have become valid are at the front
             while(!dq.isEmpty() && dq.peekFirst()[0] <= prefix[r]){
-                prev = (int) dq.pollFirst()[1];
+                l = (int) dq.pollFirst()[1];
             }
 
-            dp[r] = dp[prev] + 1;
+            dp[r] = dp[l] + 1;
 
             // for some future idx next, to be valid
-            // prefix[next] - prefix[r] >= prefix[r] - prefix[prev]
-            // prefix[next] >= 2 * prefix[r] - prefix[prev]
-            long threshold = 2 * prefix[r] - prefix[prev];
+            // prefix[next] - prefix[r] >= prefix[r] - prefix[l]
+            // prefix[next] >= 2 * prefix[r] - prefix[l]
+            long threshold = 2 * prefix[r] - prefix[l];
             
             // maintain threshold in increasing order
             while(!dq.isEmpty() && dq.peekLast()[0] >= threshold){
                 dq.pollLast();
             }
+            System.out.println(l);
             dq.offerLast(new long[]{threshold, r});
         }
         return dp[n];
