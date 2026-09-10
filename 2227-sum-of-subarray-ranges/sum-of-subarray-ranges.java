5a1318f1bd5305db7a767b2d56134d1d99be450a
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-10 18:00:24 +0530

    Time: 26 ms (44.01%) | Memory: 46.7 MB (26.51%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,17 +1,77 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        long minimum = findSumOfMinOfAllSubArrays(nums);
+        long maximum = findSumOfMaxOfAllSubArrays(nums);
+
+        return maximum - minimum;
+    }
+
+    private long findSumOfMinOfAllSubArrays(int[] arr){
+        int n = arr.length;
+        int[] prevSmallerOrEqual = new int[n];
+        int[] nextSmaller = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                st.pop();
+            }
+            prevSmallerOrEqual[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            nextSmaller[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
         long total = 0;
-        int n = nums.length;
 
         for(int i = 0; i < n; i++){
-            int min = nums[i];
-            int max = nums[i];
-            for(int j = i + 1; j < n; j++){
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
+            int left = i - prevSmallerOrEqual[i];
+            int right = nextSmaller[i] - i;
 
-                total += (max - min);
+            total += (1L * left * right * arr[i]);
+        }
+        return total;
+    }
+
+    private long findSumOfMaxOfAllSubArrays(int[] arr){
+        int n = arr.length;
+        int[] prevLargerOrEqual = new int[n];
+        int[] nextLarger = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && arr[st.peek()] < arr[i]){
+                st.pop();
             }
+            prevLargerOrEqual[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
+            }
+            nextLarger[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long total = 0;
+
+        for(int i = 0; i < n; i++){
+            int left = i - prevLargerOrEqual[i];
+            int right = nextLarger[i] - i;
+
+            total += (1L * left * right * arr[i]);
         }
         return total;
     }
