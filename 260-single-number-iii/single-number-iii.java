4ab17a425be808c1ff719d33e0e5b8d28022b9c7
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-12 20:26:25 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (40.16%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num: nums) xor ^= num;
+
+        int lsbWith1 = xor & (-xor);
+
+        int[] result = new int[2];
+
+        for(int num: nums){
+            if((num & lsbWith1) == 0){
+                result[0] ^= num;
+            }else{
+                result[1] ^= num;
+            }
+        }
+        return result;
+    }
+}
