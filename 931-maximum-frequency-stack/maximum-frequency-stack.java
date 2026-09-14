583dd673b2553f3e8e8e4bc7e1c581a99ba11133
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-14 22:13:58 +0530

    Time: 32 ms (76.04%) | Memory: 74.2 MB (5.16%) - LeetSync

---
+++ 931-maximum-frequency-stack/maximum-frequency-stack.java
@@ -0,0 +1,37 @@
+class FreqStack {
+    // val, freq
+    HashMap<Integer, Integer> freq;
+    // freq, Stack<val>
+    HashMap<Integer, Stack<Integer>> map;
+    int maxFreq;
+
+    public FreqStack() {
+        freq = new HashMap<>();
+        map = new HashMap<>();
+        maxFreq = 0;
+    }
+    
+    public void push(int val) {
+        int f = freq.getOrDefault(val, 0) + 1;
+        freq.put(val, f);
+        maxFreq = Math.max(maxFreq, f);
+
+        if(!map.containsKey(f)) map.put(f, new Stack<Integer>());
+        map.get(f).add(val);    
+    }
+    
+    public int pop() {
+        int val = map.get(maxFreq).pop();
+        freq.put(val, maxFreq-1);
+
+        if(map.get(maxFreq).size() == 0) maxFreq--;
+        return val;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
