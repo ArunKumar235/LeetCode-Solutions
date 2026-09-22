f8a65bbbb734c77abe185c3c22ed38ad20a40352
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-22 21:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (35.64%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.java
+++ 1022-unique-paths-iii/unique-paths-iii.java
@@ -1,5 +1,4 @@
 class Solution {
-
     private int m;
     private int n;
     private int paths = 0;
@@ -7,18 +6,18 @@ class Solution {
     private int totalEmpty = 0;
     private int visitedEmpty = 0;
 
-    public int uniquePathsIII(int[][] grid) {
+    public int uniquePathsIII(int[][] grid){
         m = grid.length;
         n = grid[0].length;
         visited = new boolean[m][n];
         int startRow = -1;
         int startColumn = -1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
                     startRow = i;
                     startColumn = j;
-                } else if (grid[i][j] == 0) {
+                }else if (grid[i][j] == 0){
                     totalEmpty++;
                 }
             }
@@ -27,23 +26,24 @@ class Solution {
         return paths;
     }
 
-    private void uniquePathsIII(int[][] grid, int r, int c) {
-        if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c]) return;
+    private void uniquePathsIII(int[][] grid, int r, int c){
+        if(r < 0 || r >= m || c < 0 || c >= n || visited[r][c]) return;
+        
         int value = grid[r][c];
         if (value == -1) return;
-
         visited[r][c] = true;
-        if (value == 0 || value == 1) {
-            if (value == 0) visitedEmpty++;
+        
+        if(value == 0 || value == 1){
+            if(value == 0) visitedEmpty++;
             uniquePathsIII(grid, r + 1, c);
             uniquePathsIII(grid, r, c + 1);
             uniquePathsIII(grid, r - 1, c);
             uniquePathsIII(grid, r, c - 1);
-        } else if (value == 2 && visitedEmpty == totalEmpty) {
+        }else if(value == 2 && visitedEmpty == totalEmpty){
             paths++;
         }
-        visited[r][c] = false;
-        if (value == 0) visitedEmpty--;
-    }
 
+        visited[r][c] = false;
+        if(value == 0) visitedEmpty--;
+    }
 }
