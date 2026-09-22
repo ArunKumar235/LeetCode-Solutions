40dfd98de11041213f550365176a4f74e2729596
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-22 21:26:13 +0530

    Time: 224 ms (10.99%) | Memory: 47 MB (49.10%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,79 +1,33 @@
 class Solution {
-    int INF = Integer.MAX_VALUE;
-    
-    String[] stickers;
-    int[][] count;
-
-    String target;
-    int n;
-    int[] dp;
-    
-    public int minStickers(String[] stickers,String target) {
-        n = target.length();
-        this.target = target;
+    public int minStickers(String[] stickers, String target) {
+        int n = target.length();
+        int maxMask = 1 << n;
         
-        // count[i] = char freq array for stickers[i]
-        count = new int[stickers.length][26];
+        // dp[mask] = minimum number of stickers needed to form given state mask
+        int[] dp = new int[maxMask];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
-        for(int i = 0; i < stickers.length; i++) {
-            for(char c : stickers[i].toCharArray()) {
-                count[i][c - 'a']++;
-            }
-        }
+        for(int mask = 0; mask < maxMask; mask++){
+            if(dp[mask] == Integer.MAX_VALUE) continue;
 
-        dp = new int[1 << n];
-        Arrays.fill(dp, -1);
-
-        int res = func(0);
-
-        return res == INF ? -1 : res;
-    }
-    
-    // Memoization - I've already solved this state, so don't solve it again.
-    // Pruning - This choice isn't worth exploring in the first place.
-    // we are immediately rejecting the sticker that won't be useful at the moment, 
-    // instead of exploring all stickers
-    // if the first char doesn't match, we are temporarily rejecting the sticker for current state, 
-    // we might still use it in future
-
-    private int func(int state){
-        if(state == (1 << n) - 1) return 0;
-
-        if(dp[state] != -1) return dp[state];
-
-        // find the first unset character
-        int idx = 0;
-        while((state & (1 << idx)) != 0) {
-            idx++;
-        }
-
-        int res = INF;
-
-        int targetChar = target.charAt(idx) - 'a';
-
-        for(int s = 0; s < count.length; s++) {
-            if(count[s][targetChar] == 0) continue;
-
-            int newState = state;
-            int[] stickerFreq = count[s].clone();
-            // apply the sticker
-            for(int t = idx; t < n; t++) {
-                if((newState & (1 << t)) != 0) continue;
-
-                int c = target.charAt(t) - 'a';
-
-                if(stickerFreq[c] > 0) {
-                    newState |= (1 << t);
-                    stickerFreq[c]--;
+            // try applying each sticker to current mask state
+            for(String sticker : stickers){
+                int superMask = mask;
+                
+                for(char c : sticker.toCharArray()){
+                    for(int i = 0; i < n; i++){
+                        // if target character matches and isn't already set in superMask
+                        if(c == target.charAt(i) && (superMask & (1 << i)) == 0){
+                            superMask |= (1 << i);
+                            break; // each character in sticker can cover at most one target character
+                        }
+                    }
                 }
-            }
-
-            if(newState != state) {
-                int next = func(newState);
-
-                if(next != INF) res = Math.min(res, next + 1);
+                // update transition with minimum stickers needed
+                dp[superMask] = Math.min(dp[superMask], dp[mask] + 1);
             }
         }
-        return dp[state] = res;
+        return dp[maxMask - 1] == Integer.MAX_VALUE ? -1 : dp[maxMask - 1];
     }
 }
