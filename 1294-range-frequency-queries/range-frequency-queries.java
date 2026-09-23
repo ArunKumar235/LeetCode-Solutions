0811395fa7809481caace5250cdd79fee23f11bf
Arun Kumar M <marunkumar0203@gmail.com>
2026-09-23 23:37:02 +0530

    Time: 633 ms (5.00%) | Memory: 173.1 MB (97.67%) - LeetSync

--- 1294-range-frequency-queries/range-frequency-queries.java
+++ 1294-range-frequency-queries/range-frequency-queries.java
@@ -1,18 +1,65 @@
 class RangeFreqQuery {
-    Map<Integer, TreeMap<Integer, Integer>> map = new HashMap<>();
+    int[] arr;
+    int blockSize;
+    List<Map<Integer, Integer>> blocks;
+
     public RangeFreqQuery(int[] arr) {
-        for(int i = 0; i < arr.length;i++){
-            map.putIfAbsent(arr[i], new TreeMap<>());
-            map.get(arr[i]).put(i, map.get(arr[i]).size());
+        this.arr = arr;
+
+        int n = arr.length;
+        blockSize = (int) Math.sqrt(n) + 1;
+
+        int blockCount = (n + blockSize - 1) / blockSize;
+
+        blocks = new ArrayList<>();
+        for(int i = 0; i < blockCount; i++) blocks.add(new HashMap<>());
+
+        for(int i = 0; i < n; i++){
+            int block = i / blockSize;
+
+            Map<Integer, Integer> freq = blocks.get(block);
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
         }
     }
     
     public int query(int left, int right, int value) {
-        if(!map.containsKey(value)) return 0;
-        TreeMap<Integer, Integer> nums = map.get(value);
-        Integer a = nums.ceilingKey(left), b = nums.floorKey(right);
-        if(a == null || b == null) return 0;
-        return nums.get(b) - nums.get(a) +1;
+        int count = 0;
+
+        int leftBlock = left / blockSize;
+        int rightBlock = right / blockSize;
+
+        // same block
+        if(leftBlock == rightBlock){
+            for(int i = left; i <= right; i++){
+                if(arr[i] == value){
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        // left partial block
+        int leftEnd = (leftBlock + 1) * blockSize - 1;
+        for(int i = left; i <= leftEnd; i++){
+            if(arr[i] == value){
+                count++;
+            }
+        }
+
+        // complete blocks
+        for(int block = leftBlock + 1; block < rightBlock; block++){
+            count += blocks.get(block).getOrDefault(value, 0);
+        }
+
+        // right partial block
+        int rightStart = rightBlock * blockSize;
+        for(int i = rightStart; i <= right; i++){
+            if(arr[i] == value){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
 
